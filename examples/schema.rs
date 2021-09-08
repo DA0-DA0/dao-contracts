787afdcce0165284cade3f94605de5ffaf1105bf
Jake Hartnell <jake.hartnell@gmail.com>
2021-09-07 19:52:17 -0700

    Clean up
    - Change package name to cw_dao
    - Change version to 0.1.0
    - Remove pointless setup_accounts function from tests

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
 
-use sg_dao::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_dao::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
