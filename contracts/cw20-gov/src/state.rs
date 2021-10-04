8dfdc88b0b881b00125899fd55c2db781b1e158e
ben <ben2x4@tutanota.com>
2021-10-04 10:41:26 -0700

    add delegation state

--- contracts/cw20-gov/src/state.rs
+++ contracts/cw20-gov/src/state.rs
@@ -1,5 +1,6 @@
 use cosmwasm_std::{Addr, Uint128};
-use cw_storage_plus::{SnapshotMap, Strategy};
+use cw_storage_plus::{SnapshotMap, Strategy, Map};
+use std::ops::Add;
 
 pub const VOTING_POWER: SnapshotMap<&Addr, Uint128> = SnapshotMap::new(
     "voting_power",
@@ -7,3 +8,6 @@ pub const VOTING_POWER: SnapshotMap<&Addr, Uint128> = SnapshotMap::new(
     "voting_power__changelog",
     Strategy::EveryBlock,
 );
+
+// TODO: implement this feature
+pub const DELEGATIONS: Map<&Addr,&Add> = Map::new("delegations")
