e8f397ce26a4389a6a7e682b6146fbf1e9b77f2b
ben <ben2x4@tutanota.com>
2021-10-30 15:05:45 -0700

    no-op contract

---
+++ contracts/cw-distribution/README.md
@@ -0,0 +1,3 @@
+# CW20 Extension Base
+
+This is a simple base contract for implementing cw20 extensions. It imports the execute and query messages from [CW20-Base](https://github.com/CosmWasm/cw-plus/tree/main/contracts/cw20-base) and reimplements the exact same functionality. 
---
+++ contracts/cw-distribution/src/contract.rs
@@ -0,0 +1,41 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult, Uint128,
+};
+
+use crate::msg::{
+    ExecuteMsg, InstantiateMsg, QueryMsg,
+};
+use crate::error::ContractError;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response<Empty>, ContractError> {
+   Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response<Empty>, ContractError> {
+   Ok(Response::default())
+}
+
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    to_binary(&{})
+}
+
+
+#[cfg(test)]
+mod tests {
+}
