3d2a27fd87e1575eed9a1673580c6a6ad3abb0b6
Orkun Külçe <orkunkl@users.noreply.github.com>
2021-11-25 17:21:02 +0300

    Generate schema

--- contracts/cw4-registry/examples/schema.rs
+++ contracts/cw4-registry/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw4_registry::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use cw4_registry::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg, ListGroupsResponse};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -15,4 +15,5 @@ fn main() {
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(MigrateMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(ListGroupsResponse), &out_dir);
 }
