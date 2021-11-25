fce954c2f65450e73a4b57303feb2cdc0b4e49a7
Alex Lynham <alex@lynh.am>
2021-11-25 17:36:09 +0000

    Add comment to explain what's going on here

--- contracts/cw-distribution/src/msg.rs
+++ contracts/cw-distribution/src/msg.rs
@@ -1,6 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+// NB: these are placeholders, set as structs
+// so they don't result in invalid JSON. They should ofc be enums
 #[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
 pub struct InstantiateMsg {}
 
