f4c5454e2efa9ac451666b0fedbf5b9b4a858fd3
gavin.doughtie <gavin.doughtie@gmail.com>
2021-12-02 14:13:15 -0800

    more type generation

--- types/contracts/cw3-dao/index.ts
+++ types/contracts/cw3-dao/index.ts
@@ -1,4 +1,13 @@
+// dedup emptied this file
+// export * from "./config";
 export * from "./execute_msg";
 export * from "./instantiate_msg";
+export * from "./proposal_list_response";
+export * from "./proposal_response";
+export * from "./proposal";
 export * from "./query_msg";
 export * from "./shared-types";
+// dedup emptied this file
+// export * from "./vote_info";
+export * from "./vote_list_response";
+export * from "./vote_response";
--- types/contracts/cw3-flex-multisig/instantiate_msg.d.ts
+++ types/contracts/cw3-flex-multisig/instantiate_msg.d.ts
@@ -1,3 +1,16 @@
+export type GroupMsg = ({
+instantiate_new_group: {
+code_id: number
+label: string
+voters: Member[]
+[k: string]: unknown
+}
+} | {
+use_existing_group: {
+addr: string
+[k: string]: unknown
+}
+})
 /**
  * Duration is a delta of time. You can add it to a BlockInfo or Expiration to move that further in the future. Note that an height-based Duration and a time-based Expiration cannot be combined
  */
@@ -36,8 +49,16 @@ threshold: Decimal
 export type Decimal = string
 
 export interface InstantiateMsg {
-group_addr: string
+group: GroupMsg
 max_voting_period: Duration
 threshold: Threshold
+[k: string]: unknown
+}
+/**
+ * A group member has a weight associated with them. This may all be equal, or may have meaning in the app that makes use of the group (eg. voting power)
+ */
+export interface Member {
+addr: string
+weight: number
 [k: string]: unknown
 }
