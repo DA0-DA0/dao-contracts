bc4e18e81af3803f85cdec52fc0e49e9ec7edfcc
ben <ben2x4@tutanota.com>
2022-02-02 18:25:48 -0800

    update cw3-dao and bump version

--- contracts/cw3-dao/src/contract.rs
+++ contracts/cw3-dao/src/contract.rs
@@ -140,7 +140,7 @@ pub fn instantiate(
                 admin: Some(env.contract.address.to_string()),
                 label,
                 msg: to_binary(&stake_cw20::msg::InstantiateMsg {
-                    admin: env.contract.address,
+                    admin: Some(env.contract.address),
                     unstaking_duration,
                     token_address: cw20_addr.addr(),
                 })?,
@@ -740,7 +740,7 @@ pub fn reply(deps: DepsMut, env: Env, msg: Reply) -> Result<Response, ContractEr
                         admin: Some(env.contract.address.to_string()),
                         label: env.contract.address.to_string(),
                         msg: to_binary(&stake_cw20::msg::InstantiateMsg {
-                            admin: env.contract.address,
+                            admin: Some(env.contract.address),
                             unstaking_duration,
                             token_address: cw20_addr,
                         })?,
