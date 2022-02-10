39129826578c750b8303d3ee7fa779bf973e7725
Zeke Medley <zekemedley@gmail.com>
2022-02-10 09:15:05 -0800

    fixup! Remove unneeded files.

--- contracts/address-manager/src/error.rs
+++ contracts/address-manager/src/error.rs
@@ -8,6 +8,4 @@ pub enum ContractError {
 
     #[error("Unauthorized")]
     Unauthorized {},
-    // Add any other custom errors you like here.
-    // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
