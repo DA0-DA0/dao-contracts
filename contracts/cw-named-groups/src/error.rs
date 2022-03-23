3f6c79aad0561223de222b3b4aef03811e9b231b
Noah Saso <noahsaso@gmail.com>
2022-03-23 16:36:45 -0700

    Implemented instantiation, dump, add, and remove functions.

--- contracts/cw-named-groups/src/error.rs
+++ contracts/cw-named-groups/src/error.rs
@@ -1,8 +1,17 @@
 use cosmwasm_std::StdError;
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("Invalid group: {0}")]
+    InvalidGroup(String),
+
+    #[error("Invalid address: {0}")]
+    InvalidAddress(String),
 }
