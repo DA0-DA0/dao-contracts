54a65cd7780c9d99d0b88967d93a034118d9b674
Jake Hartnell <jake.hartnell@gmail.com>
2022-04-18 19:42:58 -0700

    Update cw20-staked-balance-voting contract schema

--- contracts/cw20-staked-balance-voting/examples/schema.rs
+++ contracts/cw20-staked-balance-voting/examples/schema.rs
@@ -1,9 +1,12 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-
+use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
+use cosmwasm_std::Addr;
 use cw20_staked_balance_voting::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_core_interface::voting::{
+    InfoResponse, TotalPowerAtHeightResponse, VotingPowerAtHeightResponse,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,4 +17,14 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+
+    export_schema(&schema_for!(InfoResponse), &out_dir);
+    export_schema(&schema_for!(TotalPowerAtHeightResponse), &out_dir);
+    export_schema(&schema_for!(VotingPowerAtHeightResponse), &out_dir);
+
+    // Auto TS code generation expects the query return type as QueryNameResponse
+    // Here we map query resonses to the correct name
+    export_schema_with_title(&schema_for!(Addr), &out_dir, "DaoResponse");
+    export_schema_with_title(&schema_for!(Addr), &out_dir, "StakingContractResponse");
+    export_schema_with_title(&schema_for!(Addr), &out_dir, "TokenContractResponse");
 }
