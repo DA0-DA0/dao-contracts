7c28fc337ef08366c17019d3bf48b55e20dcd97d
Zeke Medley <zekemedley@gmail.com>
2022-05-23 14:30:58 -0700

    Add code coverage badge to README.

--- packages/cw-core-interface/src/voting.rs
+++ packages/cw-core-interface/src/voting.rs
@@ -32,3 +32,31 @@ pub struct InfoResponse {
 pub struct IsActiveResponse {
     pub active: bool,
 }
+
+mod tests {
+    /// Make sure the enum has all of the fields we expect. This will
+    /// fail to compile if not.
+    #[test]
+    fn test_macro_expansion() {
+        use cw_core_macros::{active_query, token_query, voting_query};
+        use schemars::JsonSchema;
+        use serde::{Deserialize, Serialize};
+
+        #[token_query]
+        #[voting_query]
+        #[active_query]
+        #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+        #[serde(rename_all = "snake_case")]
+        enum Query {}
+
+        let query = Query::TokenContract {};
+
+        match query {
+            Query::TokenContract {} => (),
+            Query::VotingPowerAtHeight { .. } => (),
+            Query::TotalPowerAtHeight { .. } => (),
+            Query::IsActive {} => (),
+            Query::Info {} => (),
+        }
+    }
+}
