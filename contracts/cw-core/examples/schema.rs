3d979adc196ea2d5bd70c63dede22060e2d5c363
Noah Saso <noahsaso@gmail.com>
2022-05-25 11:55:57 -0700

    Fixed schema export for cw-core.

--- contracts/cw-core/examples/schema.rs
+++ contracts/cw-core/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, CosmosMsg, Empty};
 use cw_core::{
     msg::{ExecuteMsg, InstantiateMsg, QueryMsg},
     query::{Cw20BalanceResponse, DumpStateResponse, GetItemResponse, PauseInfoResponse},
@@ -11,6 +11,7 @@ use cw_core::{
 use cw_core_interface::voting::{
     InfoResponse, TotalPowerAtHeightResponse, VotingPowerAtHeightResponse,
 };
+use cw_utils::Duration;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -28,13 +29,14 @@ fn main() {
     export_schema(&schema_for!(InfoResponse), &out_dir);
     export_schema(&schema_for!(TotalPowerAtHeightResponse), &out_dir);
     export_schema(&schema_for!(VotingPowerAtHeightResponse), &out_dir);
+    export_schema(&schema_for!(Duration), &out_dir);
 
     // Auto TS code generation expects the query return type as QueryNameResponse
     // Here we map query resonses to the correct name
     export_schema_with_title(&schema_for!(Option<Addr>), &out_dir, "AdminResponse");
     export_schema_with_title(&schema_for!(Config), &out_dir, "ConfigResponse");
     export_schema_with_title(
-        &schema_for!(Cw20BalanceResponse),
+        &schema_for!(Vec<Cw20BalanceResponse>),
         &out_dir,
         "Cw20BalancesResponse",
     );
@@ -47,4 +49,10 @@ fn main() {
     );
     export_schema_with_title(&schema_for!(Vec<String>), &out_dir, "ListItemsResponse");
     export_schema_with_title(&schema_for!(Addr), &out_dir, "VotingModuleResponse");
+    export_schema_with_title(&schema_for!(Vec<Addr>), &out_dir, "ProposalModulesResponse");
+    export_schema_with_title(
+        &schema_for!(CosmosMsg<Empty>),
+        &out_dir,
+        "CosmosMsg_for_Empty",
+    );
 }
