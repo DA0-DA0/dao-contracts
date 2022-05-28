1e6e1e44760331827546030cf5efb68f408a3fa0
Callum Anderson <callumanderson745@gmail.com>
2022-05-28 22:50:00 +0100

    Clean up tests

--- contracts/cw-native-token-voting/src/tests.rs
+++ contracts/cw-native-token-voting/src/tests.rs
@@ -1,20 +1,60 @@
 use crate::contract::{instantiate, query};
 use crate::msg::{InstantiateMsg, QueryMsg};
-use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-use cosmwasm_std::{coin, from_binary, Addr, Decimal, FullDelegation, Uint128, Validator};
+use cosmwasm_std::testing::{
+    mock_dependencies, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
+};
+use cosmwasm_std::{
+    coin, from_binary, Addr, Decimal, FullDelegation, OwnedDeps, Uint128, Validator,
+};
 use cw_core_interface::voting::VotingPowerAtHeightResponse;
+
+const ADDR1: &str = "addr1";
+const ADDR2: &str = "addr2";
+const ADDR3: &str = "addr3";
+const VALI1: &str = "vali1";
+const DENOM: &str = "ujuno";
+
+fn setup_deps() -> OwnedDeps<MockStorage, MockApi, MockQuerier> {
+    let mut deps = mock_dependencies();
+    deps.querier.update_staking(
+        "abcd",
+        &[Validator {
+            address: VALI1.to_string(),
+            commission: Decimal::percent(5),
+            max_commission: Decimal::percent(5),
+            max_change_rate: Decimal::percent(5),
+        }],
+        &[
+            FullDelegation {
+                delegator: Addr::unchecked(ADDR1),
+                validator: VALI1.to_string(),
+                amount: coin(200, DENOM),
+                can_redelegate: coin(200, DENOM),
+                accumulated_rewards: vec![],
+            },
+            FullDelegation {
+                delegator: Addr::unchecked(ADDR2),
+                validator: VALI1.to_string(),
+                amount: coin(100, DENOM),
+                can_redelegate: coin(100, DENOM),
+                accumulated_rewards: vec![],
+            },
+        ],
+    );
+    deps
+}
 
 #[test]
 fn test_instantiate() {
-    let info = mock_info("addr1", &[]);
+    let info = mock_info(ADDR1, &[]);
     let env = mock_env();
-    let mut deps = mock_dependencies();
+    let mut deps = setup_deps();
     let _res = instantiate(
         deps.as_mut(),
         env,
         info,
         InstantiateMsg {
-            token_denom: "abcd".to_string(),
+            token_denom: DENOM.to_string(),
         },
     )
     .unwrap();
@@ -22,32 +62,16 @@ fn test_instantiate() {
 
 #[test]
 fn test_query_voting_power_at_height() {
-    let info = mock_info("addr1", &[]);
+    let info = mock_info(ADDR1, &[]);
     let env = mock_env();
-    let mut deps = mock_dependencies();
-    deps.querier.update_staking(
-        "abcd",
-        &[Validator {
-            address: "vali1".to_string(),
-            commission: Decimal::percent(5),
-            max_commission: Decimal::percent(5),
-            max_change_rate: Decimal::percent(5),
-        }],
-        &[FullDelegation {
-            delegator: Addr::unchecked("addr1"),
-            validator: "vali1".to_string(),
-            amount: coin(200, "abcd"),
-            can_redelegate: coin(200, "abcd"),
-            accumulated_rewards: vec![],
-        }],
-    );
+    let mut deps = setup_deps();
 
     let _res = instantiate(
         deps.as_mut(),
         mock_env(),
         info,
         InstantiateMsg {
-            token_denom: "abcd".to_string(),
+            token_denom: DENOM.to_string(),
         },
     )
     .unwrap();
@@ -56,7 +80,7 @@ fn test_query_voting_power_at_height() {
         deps.as_ref(),
         env,
         QueryMsg::VotingPowerAtHeight {
-            address: "addr1".to_string(),
+            address: ADDR1.to_string(),
             height: None,
         },
     )
