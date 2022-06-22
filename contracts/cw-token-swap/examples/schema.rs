9f353f27d7541cbb6f778b364025089f2d7d85d1
Zeke Medley <zekemedley@gmail.com>
2022-06-21 18:53:24 -0700

    Update schema generation.

--- contracts/cw-token-swap/examples/schema.rs
+++ contracts/cw-token-swap/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use cw_escrow::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, StatusResponse};
+use cw_token_swap::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, StatusResponse};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
