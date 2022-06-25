0c6fd7dd61af843aeb726d1a76c9ebeaa884b6e4
Janita Chalam <janitachalam@gmail.com>
2022-06-24 18:14:36 -0700

    dont export querymsg

--- contracts/cw-admin-factory/examples/schema.rs
+++ contracts/cw-admin-factory/examples/schema.rs
@@ -12,5 +12,4 @@ fn main() {
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
 }
