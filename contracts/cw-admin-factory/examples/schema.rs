e025f49319201a53d78967a04d0bcaeefc49bc94
Janita Chalam <janitachalam@gmail.com>
2022-06-24 18:15:03 -0700

    remove unused

--- contracts/cw-admin-factory/examples/schema.rs
+++ contracts/cw-admin-factory/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use cw_admin_factory::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_admin_factory::msg::{ExecuteMsg, InstantiateMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
