3ffba3ab15e993546147dbd3713e6ac7ee8a98ba
Callum Anderson <callumanderson745@gmail.com>
2022-07-05 21:50:09 +0100

    Clippy

--- contracts/cw-native-staked-balance-voting/src/contract.rs
+++ contracts/cw-native-staked-balance-voting/src/contract.rs
@@ -154,10 +154,10 @@ pub fn execute_update_config(
     duration: Option<Duration>,
 ) -> Result<Response, ContractError> {
     let new_owner = new_owner
-        .map(|new_owner| deps.api.addr_validate(&*new_owner))
+        .map(|new_owner| deps.api.addr_validate(&new_owner))
         .transpose()?;
     let new_manager = new_manager
-        .map(|new_manager| deps.api.addr_validate(&*new_manager))
+        .map(|new_manager| deps.api.addr_validate(&new_manager))
         .transpose()?;
     let mut config: Config = CONFIG.load(deps.storage)?;
     if Some(info.sender.clone()) != config.owner && Some(info.sender.clone()) != config.manager {
