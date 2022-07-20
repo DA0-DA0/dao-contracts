f1ce390063ff842f801dd5757df3d34131fc920a
Harry Hull <harry.hull1@gmail.com>
2022-07-20 12:41:43 -0700

    Chain refactor + fix gas report locations

--- ci/integration_tests/src/test_harness/chain.rs
+++ ci/integration_tests/src/test_harness/chain.rs
@@ -1,28 +1,37 @@
 use anyhow::Result;
-use cosm_orc::orchestrator::cosm_orc::{CosmOrc, DeployInfo, WasmMsg};
+use cosm_orc::orchestrator::cosm_orc::{CosmOrc, WasmMsg};
 use once_cell::sync::OnceCell;
 use serde::Serialize;
 use serde_json::Value;
-use std::sync::Mutex;
+use std::{fs, sync::Mutex};
 
-static COSM_ORC: OnceCell<Mutex<CosmOrc>> = OnceCell::new();
+static COSM_ORC: OnceCell<Mutex<Chain>> = OnceCell::new();
 
 // Chain gives all the tests access to a global CosmOrc singleton
-pub struct Chain {}
+pub struct Chain {
+    contract_dir: String,
+    gas_report_out: String,
+    cosm_orc: CosmOrc,
+}
 impl Chain {
-    pub fn init(cosm_orc: CosmOrc) {
-        COSM_ORC.set(Mutex::new(cosm_orc));
+    pub fn init(cosm_orc: CosmOrc, contract_dir: String, gas_report_out: String) {
+        COSM_ORC.set(Mutex::new(Chain {
+            contract_dir,
+            gas_report_out,
+            cosm_orc,
+        }));
     }
 
-    fn get() -> &'static Mutex<CosmOrc> {
+    fn get() -> &'static Mutex<Chain> {
         COSM_ORC.get().expect("cosm-orc is not initialized")
     }
 
     // returns the deployed code_id for the given contract name
     pub fn deploy_code_id(contract_name: &str) -> u64 {
-        let cosm_orc = Self::get().lock().unwrap();
+        let chain = Self::get().lock().unwrap();
 
-        cosm_orc
+        chain
+            .cosm_orc
             .contract_map
             .get(contract_name)
             .expect("contract not stored")
@@ -31,9 +40,10 @@ impl Chain {
 
     // returns the deployed code address for the given contract name
     pub fn deploy_code_addr(contract_name: &str) -> String {
-        let cosm_orc = Self::get().lock().unwrap();
+        let chain = Self::get().lock().unwrap();
 
-        cosm_orc
+        chain
+            .cosm_orc
             .contract_map
             .get(contract_name)
             .expect("contract not stored")
@@ -43,9 +53,10 @@ impl Chain {
     }
 
     pub fn add_deploy_code_addr(contract_name: &str, contract_addr: &str) {
-        let mut cosm_orc = Self::get().lock().unwrap();
+        let mut chain = Self::get().lock().unwrap();
 
-        cosm_orc
+        chain
+            .cosm_orc
             .contract_map
             .get_mut(contract_name)
             .unwrap()
@@ -54,13 +65,14 @@ impl Chain {
 
     // Get a clean testing state by clearing out all configured contract addresses
     pub fn clear_deploys() {
-        let mut cosm_orc = Self::get().lock().unwrap();
+        let mut chain = Self::get().lock().unwrap();
 
-        for (_contract, deploy) in cosm_orc.contract_map.iter_mut() {
+        for (_contract, deploy) in chain.cosm_orc.contract_map.iter_mut() {
             deploy.address = None
         }
     }
 
+    #[track_caller]
     pub fn process_msgs<X, Y, Z>(
         contract_name: String,
         msgs: &[WasmMsg<X, Y, Z>],
@@ -70,17 +82,38 @@ impl Chain {
         Y: Serialize,
         Z: Serialize,
     {
-        let mut cosm_orc = Self::get().lock().unwrap();
-        cosm_orc.process_msgs(contract_name, msgs)
+        let mut chain = Self::get().lock().unwrap();
+        chain.cosm_orc.process_msgs(contract_name, msgs)
     }
 
+    #[track_caller]
     pub fn process_msg<X, Y, Z>(contract_name: String, msg: &WasmMsg<X, Y, Z>) -> Result<Value>
     where
         X: Serialize,
         Y: Serialize,
         Z: Serialize,
     {
-        let mut cosm_orc = Self::get().lock().unwrap();
-        cosm_orc.process_msg(contract_name, msg)
+        let mut chain = Self::get().lock().unwrap();
+        chain.cosm_orc.process_msg(contract_name, msg)
+    }
+
+    #[track_caller]
+    pub fn store_contracts() {
+        let mut chain = Self::get().lock().unwrap();
+
+        let contract_dir = chain.contract_dir.clone();
+        chain
+            .cosm_orc
+            .store_contracts(contract_dir.as_str())
+            .unwrap();
+    }
+
+    pub fn save_gas_report() {
+        let chain = Self::get().lock().unwrap();
+
+        let reports = chain.cosm_orc.profiler_reports().unwrap();
+
+        let j: Value = serde_json::from_slice(&reports[0].json_data).unwrap();
+        fs::write(chain.gas_report_out.clone(), j.to_string()).unwrap();
     }
 }
--- ci/integration_tests/src/test_harness/test_runner.rs
+++ ci/integration_tests/src/test_harness/test_runner.rs
@@ -64,18 +64,16 @@ pub fn cosm_orc_test_runner(tests: &[&TestDescAndFn]) {
 fn setup() {
     env_logger::init();
     let contract_dir = env::var("CONTRACT_DIR").expect("missing CONTRACT_DIR env var");
+    let gas_report_out = env::var("GAS_REPORT_OUT").expect("missing GAS_REPORT_OUT env var");
 
     let cfg = Config::from_yaml("config.yaml").unwrap();
     let mut cosm_orc = CosmOrc::new(cfg).add_profiler(Box::new(GasProfiler::new()));
 
-    cosm_orc.store_contracts(&contract_dir).unwrap();
+    Chain::init(cosm_orc, contract_dir, gas_report_out);
 
-    Chain::init(cosm_orc);
+    Chain::store_contracts();
 }
 
 fn teardown() {
-    // TODO: Write gas output
-    // <--
-    // <--
-    // (Do this locally for now, just to prove that I broke it and then I can fix it for real)
+    Chain::save_gas_report();
 }
