c6414b14d2031ff00efc730f5cad3ce5b8dbfd2a
ekez <zekemedley@gmail.com>
2022-08-22 18:25:10 -0700

    Update schema script to handle sub directories.

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -1,8 +1,39 @@
 START_DIR=$(pwd)
-for f in ./contracts/*
+
+cd contracts/cw-dao-core
+cargo run --example schema
+
+cd "$START_DIR"
+
+for f in ./contracts/voting-modules/*
 do
   cd "$f"
   CMD="cargo run --example schema"
   eval $CMD
   cd "$START_DIR"
 done
+
+for f in ./contracts/proposal-modules/*
+do
+  cd "$f"
+  CMD="cargo run --example schema"
+  eval $CMD
+  cd "$START_DIR"
+done
+
+for f in ./contracts/staking-rewards/*
+do
+  cd "$f"
+  CMD="cargo run --example schema"
+  eval $CMD
+  cd "$START_DIR"
+done
+
+for f in ./contracts/utils/*
+do
+  cd "$f"
+  CMD="cargo run --example schema"
+  eval $CMD
+  cd "$START_DIR"
+done
+
