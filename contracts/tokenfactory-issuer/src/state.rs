c960529c0dac0f204ee6c36f9fba819ad0451ce1
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-10-04 13:06:36 +0700

    use cw_serde on config

--- contracts/tokenfactory-issuer/src/state.rs
+++ contracts/tokenfactory-issuer/src/state.rs
@@ -1,10 +1,8 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Uint128};
 use cw_storage_plus::{Item, Map};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[cw_serde]
 pub struct Config {
     pub denom: String,
     pub owner: Addr,
