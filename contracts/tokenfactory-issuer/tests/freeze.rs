ac14289cfeccf998564000838605b6d602c1d26f
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-10-15 17:09:08 +0700

    add more freeze test

--- contracts/tokenfactory-issuer/tests/freeze.rs
+++ contracts/tokenfactory-issuer/tests/freeze.rs
@@ -36,6 +36,38 @@ fn set_freezeer_performed_by_contract_owner_should_pass() {
 }
 
 #[test]
+fn freeze_by_freezer_should_pass() {
+    let env = TestEnv::default();
+    let owner = &env.test_accs[0];
+    let non_owner = &env.test_accs[1];
+
+    env.tokenfactory_issuer
+        .set_freezer(&non_owner.address(), true, owner)
+        .unwrap();
+    env.tokenfactory_issuer.freeze(true, non_owner).unwrap();
+
+    // should be frozen after set true
+    assert!(env.tokenfactory_issuer.query_is_frozen().unwrap().is_frozen);
+
+    env.tokenfactory_issuer.freeze(false, non_owner).unwrap();
+
+    // should be unfrozen after set false
+    assert!(!env.tokenfactory_issuer.query_is_frozen().unwrap().is_frozen);
+}
+
+#[test]
+fn freeze_by_non_freezer_should_fail() {
+    let env = TestEnv::default();
+    let owner = &env.test_accs[0];
+    let err = env.tokenfactory_issuer.freeze(true, owner).unwrap_err();
+
+    assert_eq!(
+        err,
+        TokenfactoryIssuer::execute_error(ContractError::Unauthorized {})
+    );
+}
+
+#[test]
 fn set_freezeer_performed_by_non_contract_owner_should_fail() {
     let env = TestEnv::default();
     let non_owner = &env.test_accs[1];
@@ -50,6 +82,7 @@ fn set_freezeer_performed_by_non_contract_owner_should_fail() {
         TokenfactoryIssuer::execute_error(ContractError::Unauthorized {})
     );
 }
+
 #[test]
 fn set_freezeer_to_false_should_remove_it_from_state() {
     let env = TestEnv::default();
