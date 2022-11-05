5cc0158ec0e7f7f3a77725e72cd95fe5d59050a1
bekauz <bekauz@protonmail.com>
2022-11-05 19:10:41 +0100

    prop-multiple adversarial tests

--- contracts/proposal/cwd-proposal-multiple/src/testing/adversarial_tests.rs
+++ contracts/proposal/cwd-proposal-multiple/src/testing/adversarial_tests.rs
@@ -1,10 +1,14 @@
 use cosmwasm_std::{Addr, CosmosMsg};
-use cw_multi_test::App;
-use cwd_voting::multiple_choice::{MultipleChoiceOption, MultipleChoiceOptions};
+use cw_multi_test::{App, Executor, next_block};
+use cwd_voting::multiple_choice::{MultipleChoiceOption, MultipleChoiceOptions, MultipleChoiceVote};
+use cwd_voting::status::Status;
+use cwd_voting::voting::MultipleChoiceVotes;
+use crate::ContractError;
+use crate::msg::ExecuteMsg;
 use crate::testing::execute::{make_proposal, mint_cw20s};
-use crate::testing::instantiate::{_get_default_token_dao_proposal_module_instantiate, instantiate_with_staked_balances_governance};
-use crate::testing::queries::{query_dao_token, query_multiple_proposal_module};
-use crate::testing::tests::CREATOR_ADDR;
+use crate::testing::instantiate::{_get_default_token_dao_proposal_module_instantiate, instantiate_with_multiple_staked_balances_governance, instantiate_with_staked_balances_governance};
+use crate::testing::queries::{query_dao_token, query_multiple_proposal_module, query_proposal};
+use crate::testing::tests::{ALTERNATIVE_ADDR, CREATOR_ADDR};
 
 struct CommonTest {
     app: App,
@@ -16,7 +20,7 @@ struct CommonTest {
 fn setup_test(messages: Vec<CosmosMsg>) -> CommonTest {
     let mut app = App::default();
     let instantiate = _get_default_token_dao_proposal_module_instantiate(&mut app);
-    let core_addr = instantiate_with_staked_balances_governance(&mut app, instantiate, None);
+    let core_addr = instantiate_with_multiple_staked_balances_governance(&mut app, instantiate, None);
     let proposal_module = query_multiple_proposal_module(&app, &core_addr);
     let gov_token = query_dao_token(&app, &core_addr);
 
@@ -57,16 +61,180 @@ fn test_execute_proposal_open() {
         proposal_id,
     } = setup_test(vec![]);
 
+    app.update_block(next_block);
 
+    // assert proposal is open
+    let prop = query_proposal(&app, &proposal_module, proposal_id);
+    assert_eq!(prop.proposal.status, Status::Open);
 
+    // attempt to execute and assert that it fails
+    let err = app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module,
+        &ExecuteMsg::Execute { proposal_id },
+        &[],
+    )
+    .unwrap_err()
+    .downcast()
+    .unwrap();
+
+    assert!(matches!(err, ContractError::NotPassed {}))
 }
 
 #[test]
 fn test_execute_proposal_rejected_closed() {
-    unimplemented!()
+    let CommonTest {
+        mut app,
+        core_addr: _,
+        proposal_module,
+        gov_token: _,
+        proposal_id,
+    } = setup_test(vec![]);
+
+    app.update_block(next_block);
+
+    // assert proposal is open
+    let prop = query_proposal(&app, &proposal_module, proposal_id);
+    assert_eq!(prop.proposal.status, Status::Open);
+
+    // Vote on both options to reject the proposal
+    let vote = MultipleChoiceVote {
+        option_id: 0,
+    };
+    app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Vote {
+            proposal_id,
+            vote
+        },
+        &[],
+    )
+    .unwrap();
+
+    let vote = MultipleChoiceVote {
+        option_id: 1,
+    };
+    app.execute_contract(
+        Addr::unchecked(ALTERNATIVE_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Vote {
+            proposal_id,
+            vote
+        },
+        &[],
+    )
+    .unwrap();
+
+    app.update_block(next_block);
+
+    let prop = query_proposal(&app, &proposal_module, proposal_id);
+    assert_eq!(prop.proposal.status, Status::Rejected);
+
+    // attempt to execute and assert that it fails
+    let err = app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Execute { proposal_id },
+        &[],
+    )
+    .unwrap_err()
+    .downcast()
+    .unwrap();
+
+    assert!(matches!(err, ContractError::NotPassed {}));
+
+    app.update_block(next_block);
+
+    // close the proposal
+    app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Close { proposal_id },
+        &[],
+    )
+    .unwrap();
+
+    // assert prop is closed and attempt to execute it
+    let prop = query_proposal(&app, &proposal_module, proposal_id);
+    assert_eq!(prop.proposal.status, Status::Closed);
+
+    let err = app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module,
+        &ExecuteMsg::Execute { proposal_id },
+        &[],
+    )
+    .unwrap_err()
+    .downcast()
+    .unwrap();
+    assert!(matches!(err, ContractError::NotPassed {}));
 }
 
 #[test]
 fn test_execute_proposal_more_than_once() {
-    unimplemented!()
+    let CommonTest {
+        mut app,
+        core_addr: _,
+        proposal_module,
+        gov_token: _,
+        proposal_id,
+    } = setup_test(vec![]);
+
+    app.update_block(next_block);
+
+    // get the proposal to pass
+    let vote = MultipleChoiceVote {
+        option_id: 0,
+    };
+    app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Vote {
+            proposal_id,
+            vote
+        },
+        &[],
+    )
+    .unwrap();
+    app.execute_contract(
+        Addr::unchecked(ALTERNATIVE_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Vote {
+            proposal_id,
+            vote
+        },
+        &[],
+    )
+    .unwrap();
+
+    app.update_block(next_block);
+
+    let prop = query_proposal(&app, &proposal_module, proposal_id);
+    assert_eq!(prop.proposal.status, Status::Passed);
+
+    // execute the proposal
+    app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module.clone(),
+        &ExecuteMsg::Execute { proposal_id },
+        &[],
+    )
+    .unwrap();
+
+    app.update_block(next_block);
+
+    let prop = query_proposal(&app, &proposal_module, proposal_id);
+    assert_eq!(prop.proposal.status, Status::Executed);
+
+    let err = app.execute_contract(
+        Addr::unchecked(CREATOR_ADDR),
+        proposal_module,
+        &ExecuteMsg::Execute { proposal_id },
+        &[],
+    )
+    .unwrap_err()
+    .downcast()
+    .unwrap();
+    assert!(matches!(err, ContractError::NotPassed {}));
 }
