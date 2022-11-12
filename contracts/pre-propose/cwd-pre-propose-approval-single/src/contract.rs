ee9b962d9e2e03ab7d8298178b399e20f9422eb0
Jake Hartnell <jake.hartnell@gmail.com>
2022-11-11 18:36:47 -0800

    Implement basic approval flow logic and checks

--- contracts/pre-propose/cwd-pre-propose-approval-single/src/contract.rs
+++ contracts/pre-propose/cwd-pre-propose-approval-single/src/contract.rs
@@ -2,16 +2,20 @@ use cosmwasm_schema::cw_serde;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    Binary, CosmosMsg, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    to_binary, Binary, CosmosMsg, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    WasmMsg,
 };
 use cw2::set_contract_version;
+use cwd_interface::voting::{Query as CwCoreQuery, VotingPowerAtHeightResponse};
 use cwd_pre_propose_base::{
     error::PreProposeError,
     msg::{ExecuteMsg as ExecuteBase, InstantiateMsg as InstantiateBase, QueryMsg as QueryBase},
     state::PreProposeContract,
 };
+use cwd_proposal_single::msg::ExecuteMsg as ProposalSingleExecuteMsg;
+use cwd_voting::deposit::DepositRefundPolicy;
 
-use crate::state::APPROVER;
+use crate::state::{APPROVER, CURRENT_ID, PENDING_PROPOSALS};
 
 pub(crate) const CONTRACT_NAME: &str = "crates.io:cwd-pre-propose-approval-single";
 pub(crate) const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -54,13 +58,11 @@ pub type QueryMsg = QueryBase<QueryExt>;
 /// `proposer` field. The module will fill this in based on the sender
 /// of the external message.
 #[cw_serde]
-pub enum ProposeMessageInternal {
-    Propose {
-        title: String,
-        description: String,
-        msgs: Vec<CosmosMsg<Empty>>,
-        proposer: Option<String>,
-    },
+pub struct ProposeMessageInternal {
+    title: String,
+    description: String,
+    msgs: Vec<CosmosMsg<Empty>>,
+    proposer: Option<String>,
 }
 
 type PrePropose = PreProposeContract<InstantiateExt, ExecuteExt, QueryExt, ProposeMessage>;
@@ -72,8 +74,12 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, PreProposeError> {
+    // Validate and save approver address
     let approver = deps.api.addr_validate(&msg.extension.approver)?;
     APPROVER.save(deps.storage, &approver)?;
+
+    // Initialize first proposal ID
+    CURRENT_ID.save(deps.storage, &1)?;
 
     let resp = PrePropose::default().instantiate(deps.branch(), env, info, msg)?;
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
@@ -98,8 +104,8 @@ pub fn execute(
         }
         // Extension
         ExecuteMsg::Extension { msg } => match msg {
-            ExecuteExt::Approve { id } => execute_approve(deps, env, info, id),
-            ExecuteExt::Reject { id } => execute_reject(deps, env, info, id),
+            ExecuteExt::Approve { id } => execute_approve(deps, info, id),
+            ExecuteExt::Reject { id } => execute_reject(deps, info, id),
         },
         // Default pre-propose-base behavior for all other messages
         _ => PrePropose::default().execute(deps, env, info, msg),
@@ -112,22 +118,74 @@ pub fn execute_propose(
     info: MessageInfo,
     msg: ProposeMessage,
 ) -> Result<Response, PreProposeError> {
-    /*
-     * Should
-     * - Store prop with ID and original proposer
-     * - Fire off hook? (if registered... we don't know where to send it...)
-     */
+    // TODO do with less code duplication from pre-propose-base?
+    // Check that sender can propose
+    let config = PrePropose::default().config.load(deps.storage)?;
+    if !config.open_proposal_submission {
+        let dao = PrePropose::default().dao.load(deps.storage)?;
+        let voting_power: VotingPowerAtHeightResponse = deps.querier.query_wasm_smart(
+            dao.into_string(),
+            &CwCoreQuery::VotingPowerAtHeight {
+                address: info.sender.to_string(),
+                height: None,
+            },
+        )?;
+        if voting_power.power.is_zero() {
+            return Err(PreProposeError::NotMember {});
+        }
+    }
 
-    // Can sender propose?
+    // Load current id
+    let id = CURRENT_ID.load(deps.storage)?;
 
-    // Handle deposit logic?
+    // Save the proposal as pending
+    // TODO this match is qwkward?
+    match msg {
+        ProposeMessage::Propose {
+            title,
+            description,
+            msgs,
+        } => PENDING_PROPOSALS.save(
+            deps.storage,
+            id,
+            &ProposeMessageInternal {
+                title,
+                description,
+                msgs,
+                proposer: Some(info.sender.to_string()),
+            },
+        )?,
+    }
 
-    Ok(Response::default())
+    // Handle deposit if configured
+    let deposit_messages = if let Some(ref deposit_info) = config.deposit_info {
+        deposit_info.check_native_deposit_paid(&info)?;
+        deposit_info.get_take_deposit_messages(&info.sender, &env.contract.address)?
+    } else {
+        vec![]
+    };
+
+    // Increment next id
+    CURRENT_ID.save(deps.storage, &(id + 1))?;
+
+    // // TODO Prepare hooks msg to notify approver
+    // // TODO Figure out what should be in this message
+    // let hooks_msgs = PrePropose::default()
+    //     .proposal_submitted_hooks
+    //     .prepare_hooks(deps.storage, |a| {
+    //         let execute = WasmMsg::Execute {
+    //             contract_addr: a.into_string(),
+    //             msg: to_binary(&msg)?,
+    //             funds: vec![],
+    //         };
+    //         Ok(SubMsg::new(execute))
+    //     })?;
+
+    Ok(Response::default().add_messages(deposit_messages))
 }
 
 pub fn execute_approve(
     deps: DepsMut,
-    env: Env,
     info: MessageInfo,
     id: u64,
 ) -> Result<Response, PreProposeError> {
@@ -137,27 +195,29 @@ pub fn execute_approve(
         return Err(PreProposeError::Unauthorized {});
     }
 
-    // Check proposal id exists
+    // Check proposal id exists and load proposal
+    let proposal = PENDING_PROPOSALS.load(deps.storage, id)?;
 
-    // TODO submit proposal
-    // ExecuteInternal::Propose {
-    //         msg: ProposeMessageInternal::Propose {
-    //             // Fill in proposer based on message sender.
-    //             proposer: Some(info.sender.to_string()),
-    //             title,
-    //             description,
-    //             msgs,
-    //         },
-    //     },
+    let proposal_module = PrePropose::default().proposal_module.load(deps.storage)?;
+    let propose_messsage = WasmMsg::Execute {
+        contract_addr: proposal_module.into_string(),
+        msg: to_binary(&ProposalSingleExecuteMsg::Propose {
+            title: proposal.title,
+            description: proposal.description,
+            msgs: proposal.msgs,
+            proposer: proposal.proposer,
+        })?,
+        funds: vec![],
+    };
 
-    // Handle deposit logic?
+    // Remove proposal
+    PENDING_PROPOSALS.remove(deps.storage, id);
 
-    Ok(Response::default())
+    Ok(Response::default().add_message(propose_messsage))
 }
 
 pub fn execute_reject(
     deps: DepsMut,
-    env: Env,
     info: MessageInfo,
     id: u64,
 ) -> Result<Response, PreProposeError> {
@@ -168,10 +228,48 @@ pub fn execute_reject(
     }
 
     // Check proposal id exists
+    PENDING_PROPOSALS.load(deps.storage, id)?;
 
-    // Handle deposit logic?
+    // Remove proposal
+    PENDING_PROPOSALS.remove(deps.storage, id);
 
-    Ok(Response::default())
+    // Handle deposit logic
+    match PrePropose::default().deposits.may_load(deps.storage, id)? {
+        Some((deposit_info, proposer)) => {
+            let messages = if let Some(ref deposit_info) = deposit_info {
+                // Refund can be issued if proposal if it is going to
+                // closed or executed.
+                let should_refund_to_proposer = (deposit_info.refund_policy
+                    == DepositRefundPolicy::Always)
+                    || (deposit_info.refund_policy != DepositRefundPolicy::Never);
+
+                if should_refund_to_proposer {
+                    deposit_info.get_return_deposit_message(&proposer)?
+                } else {
+                    // If the proposer doesn't get the deposit, the DAO does.
+                    let dao = PrePropose::default().dao.load(deps.storage)?;
+                    deposit_info.get_return_deposit_message(&dao)?
+                }
+            } else {
+                // No deposit info for this proposal. Nothing to do.
+                vec![]
+            };
+
+            Ok(Response::default()
+                .add_attribute("method", "proposal_rejected")
+                .add_attribute("proposal", id.to_string())
+                .add_attribute("deposit_info", to_binary(&deposit_info)?.to_string())
+                .add_messages(messages))
+        }
+
+        // If we do not have a deposit for this proposal it was
+        // likely created before we were added to the proposal
+        // module. In that case, it's not our problem and we just
+        // do nothing.
+        None => Ok(Response::default()
+            .add_attribute("method", "proposal_rejected")
+            .add_attribute("proposal", id.to_string())),
+    }
 }
 
 pub fn execute_add_approver_hook(
@@ -218,6 +316,7 @@ pub fn execute_remove_approver_hook(
     Ok(Response::default())
 }
 
+// TODO queries
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
--- contracts/pre-propose/cwd-pre-propose-approval-single/src/state.rs
+++ contracts/pre-propose/cwd-pre-propose-approval-single/src/state.rs
@@ -4,4 +4,5 @@ use cw_storage_plus::{Item, Map};
 use crate::contract::ProposeMessageInternal;
 
 pub const APPROVER: Item<Addr> = Item::new("approver");
-pub const PROPOSALS: Map<u64, ProposeMessageInternal> = Map::new("pending_proposals");
+pub const CURRENT_ID: Item<u64> = Item::new("current_id");
+pub const PENDING_PROPOSALS: Map<u64, ProposeMessageInternal> = Map::new("pending_proposals");
