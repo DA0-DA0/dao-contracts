6d8035dc9a83b1cbe3234faa02738b8d3c4ed6db
Bao <baomai@berkeley.edu>
2022-11-22 12:42:45 -0800

    init cwd-proposal-delegate

---
+++ contracts/proposal/cwd-proposal-delegate/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cwd_proposal_delegate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
