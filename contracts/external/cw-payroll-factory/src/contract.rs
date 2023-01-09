0786fcf3cf1c2016c2c00e1e510d86b7e916e9e0
Jake Hartnell <jake.hartnell@gmail.com>
2023-01-09 15:25:49 -0800

    Implement factory contract queries

--- contracts/external/cw-payroll-factory/src/contract.rs
+++ contracts/external/cw-payroll-factory/src/contract.rs
@@ -1,19 +1,24 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult, SubMsg, WasmMsg,
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Reply, Response, StdResult, SubMsg,
+    WasmMsg,
 };
 
 use cw2::set_contract_version;
 use cw_payroll::msg::InstantiateMsg as PayrollInstantiateMsg;
+use cw_storage_plus::Bound;
 use cw_utils::parse_reply_instantiate_data;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use crate::state::{vesting_contracts, VestingContract, TMP_CONTRACT_INFO};
 
-pub(crate) const CONTRACT_NAME: &str = "crates.io:cw-admin-factory";
+pub(crate) const CONTRACT_NAME: &str = "crates.io:cw-payroll-factory";
 pub(crate) const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const INSTANTIATE_CONTRACT_REPLY_ID: u64 = 0;
+pub const DEFAULT_LIMIT: u32 = 10;
+pub const MAX_LIMIT: u32 = 50;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -30,7 +35,7 @@ pub fn instantiate(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
+    deps: DepsMut,
     env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
@@ -40,12 +45,13 @@ pub fn execute(
             instantiate_msg: msg,
             code_id,
             label,
-        } => instantiate_contract(env, info, msg, code_id, label),
+        } => instantiate_contract(env, deps, info, msg, code_id, label),
     }
 }
 
 pub fn instantiate_contract(
     _env: Env,
+    deps: DepsMut,
     info: MessageInfo,
     instantiate_msg: PayrollInstantiateMsg,
     code_id: u64,
@@ -61,21 +67,82 @@ pub fn instantiate_contract(
     };
 
     let msg = SubMsg::reply_on_success(instantiate, INSTANTIATE_CONTRACT_REPLY_ID);
+
+    let instantiator = deps
+        .api
+        .addr_validate(&instantiate_msg.owner.unwrap_or(info.sender.to_string()))?;
+    let recipient = deps.api.addr_validate(&instantiate_msg.params.recipient)?;
+
+    // Save tmp contract info for use in reply
+    TMP_CONTRACT_INFO.save(deps.storage, &(recipient, instantiator))?;
+
     Ok(Response::default()
         .add_attribute("action", "instantiate_cw_payroll")
         .add_submessage(msg))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        // QueryMsg::ListVestingPayments { start_after, limit } => to_binary(&paginate_map_values(
-        //      deps,
-        //     &VESTING_PAYMENTS,
-        //     start_after,
-        //     limit,
-        //     Order::Descending,
-        // )?),
+        QueryMsg::ListVestingContracts { start_after, limit } => {
+            let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+            let start = start_after.as_deref().map(Bound::exclusive);
+
+            let res: Vec<VestingContract> = vesting_contracts()
+                .range(deps.storage, start, None, Order::Ascending)
+                .take(limit)
+                .map(|vc| Ok::<VestingContract, ContractError>(vc?.1))
+                .flatten()
+                .collect();
+
+            Ok(to_binary(&res)?)
+        }
+        QueryMsg::ListVestingContractsByInstantiator {
+            instantiator,
+            start_after,
+            limit,
+        } => {
+            let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+            let start = start_after.map(Bound::<String>::exclusive);
+
+            // Validate owner address
+            deps.api.addr_validate(&instantiator)?;
+
+            let res: Vec<VestingContract> = vesting_contracts()
+                .idx
+                .owner
+                .prefix(instantiator)
+                .range(deps.storage, start, None, Order::Ascending)
+                .take(limit)
+                .map(|vc| Ok::<VestingContract, ContractError>(vc?.1))
+                .flatten()
+                .collect();
+
+            Ok(to_binary(&res)?)
+        }
+        QueryMsg::ListVestingContractsByRecipient {
+            recipient,
+            start_after,
+            limit,
+        } => {
+            let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+            let start = start_after.map(Bound::<String>::exclusive);
+
+            // Validate recipient address
+            deps.api.addr_validate(&recipient)?;
+
+            let res: Vec<VestingContract> = vesting_contracts()
+                .idx
+                .recipient
+                .prefix(recipient)
+                .range(deps.storage, start, None, Order::Ascending)
+                .take(limit)
+                .map(|vc| Ok::<VestingContract, ContractError>(vc?.1))
+                .flatten()
+                .collect();
+
+            Ok(to_binary(&res)?)
+        }
     }
 }
 
@@ -85,6 +152,22 @@ pub fn reply(deps: DepsMut, _env: Env, msg: Reply) -> Result<Response, ContractE
         INSTANTIATE_CONTRACT_REPLY_ID => {
             let res = parse_reply_instantiate_data(msg)?;
             let contract_addr = deps.api.addr_validate(&res.contract_address)?;
+
+            let (recipient, instantiator) = TMP_CONTRACT_INFO.load(deps.storage)?;
+
+            // Save vesting contract payment info
+            vesting_contracts().save(
+                deps.storage,
+                contract_addr.as_str().clone(),
+                &VestingContract {
+                    owner: instantiator.to_string(),
+                    recipient: recipient.to_string(),
+                    contract: contract_addr.clone().to_string(),
+                },
+            )?;
+
+            // Clear tmp contract info
+            TMP_CONTRACT_INFO.remove(deps.storage);
 
             Ok(Response::default().add_attribute("new_payroll_contract", contract_addr))
         }
