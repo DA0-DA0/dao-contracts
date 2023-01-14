dc6280986c1fd924052340c41d72dc92920ac9d0
Jake Hartnell <jake.hartnell@gmail.com>
2023-01-13 18:26:58 -0800

    Update schema

--- typescript/contracts/cw-payroll-factory/bundle.ts
+++ typescript/contracts/cw-payroll-factory/bundle.ts
@@ -4,10 +4,10 @@
 * and run the @cosmwasm/ts-codegen generate command to regenerate this file.
 */
 
-import * as _20 from "./CwPayrollFactory.types";
-import * as _21 from "./CwPayrollFactory.client";
+import * as _22 from "./CwPayrollFactory.types";
+import * as _23 from "./CwPayrollFactory.client";
 export namespace contracts {
-  export const CwPayrollFactory = { ..._20,
-    ..._21
+  export const CwPayrollFactory = { ..._22,
+    ..._23
   };
 }
