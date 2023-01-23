1f9586aecad36e8b2c44c588dc0403904b5301b6
Art3mix <r3mix.il@gmail.com>
2023-01-23 22:29:37 +0200

    added tests

--- contracts/external/dao-migrator/src/testing/setup.rs
+++ contracts/external/dao-migrator/src/testing/setup.rs
@@ -1,45 +1,36 @@
-use cosmwasm_std::{to_binary, Addr, Uint128, WasmMsg};
-use cw20::Cw20Coin;
+use std::borrow::BorrowMut;
+
+use cosmwasm_std::{to_binary, Addr, WasmMsg};
 use cw_multi_test::{next_block, App, AppResponse, Executor};
 use dao_interface::{Admin, ModuleInstantiateInfo};
-use dao_testing::contracts::{
-    dao_core_contract, dao_voting_cw4_contract, proposal_single_contract,
-};
 
 use crate::{
-    testing::helpers::{dao_voting_cw20_staked_contract, v2_cw20_stake_contract},
-    types::{MigrationParams, V1CodeIds, V2CodeIds},
+    testing::helpers::get_module_addrs,
+    types::{MigrationParams, V1CodeIds},
 };
 
-use super::helpers::{migrator_contract, InitDaoDataV1, SENDER_ADDR};
+use super::helpers::{
+    get_cw20_init_msg, get_cw4_init_msg, get_v1_code_ids, get_v2_code_ids, migrator_contract,
+    set_cw20_to_dao, ExecuteParams, ModuleAddrs, VotingType, SENDER_ADDR,
+};
 
-/// Instantiate a basic DAO with proposal and voting modules.
-pub fn init_dao_v1(mut app: App, data: Option<InitDaoDataV1>) -> (App, Addr, Addr, V1CodeIds) {
-    let data = data.unwrap_or_default();
-    let sender = Addr::unchecked(SENDER_ADDR);
+pub fn init_v1(app: &mut App, sender: Addr, voting_type: VotingType) -> (Addr, V1CodeIds) {
+    let (code_ids, v1_code_ids) = get_v1_code_ids(app);
 
-    // Store v1 codes
-    let core_code = app.store_code(data.core_code);
-    let proposal_code = app.store_code(data.proposal_code);
-    let cw20_code = app.store_code(data.cw20_code);
-    let cw20_stake_code = app.store_code(data.cw20_stake_code);
-    let cw20_voting_code = app.store_code(data.cw20_voting_code);
-
-    let v1_code_ids = V1CodeIds {
-        proposal_single: proposal_code,
-        cw4_voting: 9999,
-        cw20_stake: cw20_stake_code,
-        cw20_staked_balances_voting: cw20_voting_code,
+    let (voting_code_id, msg) = match voting_type {
+        VotingType::Cw4 => (
+            code_ids.cw4_voting,
+            to_binary(&get_cw4_init_msg(code_ids.clone())).unwrap(),
+        ),
+        VotingType::Cw20 => (
+            code_ids.cw20_voting,
+            to_binary(&get_cw20_init_msg(code_ids.clone())).unwrap(),
+        ),
     };
-
-    let initial_balances = vec![Cw20Coin {
-        address: SENDER_ADDR.to_string(),
-        amount: Uint128::new(2),
-    }];
 
     let core_addr = app
         .instantiate_contract(
-            core_code,
+            code_ids.core,
             sender.clone(),
             &cw_core_v1::msg::InstantiateMsg {
                 admin: Some(SENDER_ADDR.to_string()),
@@ -49,28 +40,13 @@ pub fn init_dao_v1(mut app: App, data: Option<InitDaoDataV1>) -> (App, Addr, Add
                 automatically_add_cw20s: false,
                 automatically_add_cw721s: true,
                 voting_module_instantiate_info: cw_core_v1::msg::ModuleInstantiateInfo {
-                    code_id: cw20_voting_code,
-                    msg: to_binary(&dao_voting_cw20_staked::msg::InstantiateMsg {
-                        active_threshold: None,
-                        token_info: dao_voting_cw20_staked::msg::TokenInfo::New {
-                            code_id: cw20_code,
-                            label: "token".to_string(),
-                            name: "name".to_string(),
-                            symbol: "symbol".to_string(),
-                            decimals: 6,
-                            initial_balances,
-                            marketing: None,
-                            staking_code_id: cw20_stake_code,
-                            unstaking_duration: None,
-                            initial_dao_balance: Some(Uint128::new(100)),
-                        },
-                    })
-                    .unwrap(),
+                    code_id: voting_code_id,
+                    msg,
                     admin: cw_core_v1::msg::Admin::CoreContract {},
                     label: "voting".to_string(),
                 },
                 proposal_modules_instantiate_info: vec![cw_core_v1::msg::ModuleInstantiateInfo {
-                    code_id: proposal_code,
+                    code_id: code_ids.proposal_single,
                     msg: to_binary(&cw_proposal_single_v1::msg::InstantiateMsg {
                         threshold: voting_v1::Threshold::AbsolutePercentage {
                             percentage: voting_v1::PercentageThreshold::Majority {},
@@ -96,8 +72,10 @@ pub fn init_dao_v1(mut app: App, data: Option<InitDaoDataV1>) -> (App, Addr, Add
         )
         .unwrap();
 
+    app.update_block(next_block);
+
     app.execute(
-        sender.clone(),
+        sender,
         WasmMsg::UpdateAdmin {
             contract_addr: core_addr.to_string(),
             admin: core_addr.to_string(),
@@ -106,164 +84,61 @@ pub fn init_dao_v1(mut app: App, data: Option<InitDaoDataV1>) -> (App, Addr, Add
     )
     .unwrap();
 
-    // Get modules addrs
-    let proposal_addr = {
-        let modules: Vec<Addr> = app
-            .wrap()
-            .query_wasm_smart(
-                &core_addr,
-                &cw_core_v1::msg::QueryMsg::ProposalModules {
-                    start_at: None,
-                    limit: None,
-                },
-            )
-            .unwrap();
-        assert!(modules.len() == 1);
-        modules.into_iter().next().unwrap()
+    (core_addr, v1_code_ids)
+}
+
+/// Instantiate a basic DAO with proposal and voting modules.
+pub fn setup_dao_v1(voting_type: VotingType) -> (App, ModuleAddrs, V1CodeIds) {
+    let mut app = App::default();
+    let sender = Addr::unchecked(SENDER_ADDR);
+
+    let (core_addr, v1_code_ids) = init_v1(app.borrow_mut(), sender.clone(), voting_type.clone());
+    let module_addrs = get_module_addrs(app.borrow_mut(), core_addr);
+
+    match voting_type {
+        VotingType::Cw4 => (),
+        VotingType::Cw20 => set_cw20_to_dao(app.borrow_mut(), sender, module_addrs.clone()),
     };
-    let voting_addr: Addr = app
-        .wrap()
-        .query_wasm_smart(&core_addr, &cw_core_v1::msg::QueryMsg::VotingModule {})
-        .unwrap();
-    let staking_addr: Addr = app
-        .wrap()
-        .query_wasm_smart(
-            &voting_addr,
-            &dao_voting_cw20_staked::msg::QueryMsg::StakingContract {},
-        )
-        .unwrap();
-    let token_addr: Addr = app
-        .wrap()
-        .query_wasm_smart(
-            &voting_addr,
-            &dao_voting_cw20_staked::msg::QueryMsg::TokenContract {},
-        )
-        .unwrap();
 
-    // Stake token
-    app.execute_contract(
-        sender.clone(),
-        token_addr.clone(),
-        &cw20::Cw20ExecuteMsg::Send {
-            contract: staking_addr.to_string(),
-            amount: Uint128::new(1),
-            msg: to_binary(&cw20_stake::msg::ReceiveMsg::Stake {}).unwrap(),
-        },
-        &[],
-    )
-    .unwrap();
-    app.update_block(next_block);
-
-    // ----
-    // create a proposal and add tokens to the treasury.
-    // ----
-
-    app.execute_contract(
-        sender.clone(),
-        proposal_addr.clone(),
-        &cw_proposal_single_v1::msg::ExecuteMsg::Propose {
-            title: "t".to_string(),
-            description: "d".to_string(),
-            msgs: vec![WasmMsg::Execute {
-                contract_addr: core_addr.to_string(),
-                msg: to_binary(&cw_core_v1::msg::ExecuteMsg::UpdateCw20List {
-                    to_add: vec![token_addr.to_string()],
-                    to_remove: vec![],
-                })
-                .unwrap(),
-                funds: vec![],
-            }
-            .into()],
-        },
-        &[],
-    )
-    .unwrap();
-
-    app.execute_contract(
-        sender.clone(),
-        proposal_addr.clone(),
-        &cw_proposal_single_v1::msg::ExecuteMsg::Vote {
-            proposal_id: 1,
-            vote: voting_v1::Vote::Yes,
-        },
-        &[],
-    )
-    .unwrap();
-
-    app.execute_contract(
-        sender,
-        proposal_addr.clone(),
-        &cw_proposal_single_v1::msg::ExecuteMsg::Execute { proposal_id: 1 },
-        &[],
-    )
-    .unwrap();
-
-    let tokens: Vec<cw_core_v1::query::Cw20BalanceResponse> = app
-        .wrap()
-        .query_wasm_smart(
-            &core_addr,
-            &cw_core_v1::msg::QueryMsg::Cw20Balances {
-                start_at: None,
-                limit: None,
-            },
-        )
-        .unwrap();
-    assert_eq!(
-        tokens,
-        vec![cw_core_v1::query::Cw20BalanceResponse {
-            addr: token_addr,
-            balance: Uint128::new(100),
-        }]
-    );
-
-    (app, core_addr, proposal_addr, v1_code_ids)
+    (app, module_addrs, v1_code_ids)
 }
 
 pub fn execute_migration(
     app: &mut App,
-    core_addr: &str,
-    proposal_addr: &str,
+    module_addrs: &ModuleAddrs,
     v1_code_ids: V1CodeIds,
+    params: Option<ExecuteParams>,
 ) -> Result<AppResponse, anyhow::Error> {
     let sender = Addr::unchecked(SENDER_ADDR);
-    let core_addr = Addr::unchecked(core_addr);
-    let proposal_addr = Addr::unchecked(proposal_addr);
     let migrator_code_id = app.store_code(migrator_contract());
-    let v2_core_code_id = app.store_code(dao_core_contract());
-    let v2_proposal_code = app.store_code(proposal_single_contract());
-    let v2_cw4_voting = app.store_code(dao_voting_cw4_contract());
-    let v2_cw20_voting = app.store_code(dao_voting_cw20_staked_contract());
-    let v2_cw20_stake = app.store_code(v2_cw20_stake_contract());
-
-    let v2_code_ids = V2CodeIds {
-        proposal_single: v2_proposal_code,
-        cw4_voting: v2_cw4_voting,
-        cw20_stake: v2_cw20_stake,
-        cw20_staked_balances_voting: v2_cw20_voting,
-    };
+    let (new_code_ids, v2_code_ids) = get_v2_code_ids(app);
+    let params = params.unwrap_or_else(|| ExecuteParams {
+        sub_daos: Some(vec![]),
+        migrate_cw20: Some(true),
+    });
 
     app.execute_contract(
         sender.clone(),
-        proposal_addr.clone(),
+        module_addrs.proposal.clone(),
         &cw_proposal_single_v1::msg::ExecuteMsg::Propose {
             title: "t2".to_string(),
             description: "d2".to_string(),
             msgs: vec![
                 WasmMsg::Migrate {
-                    contract_addr: core_addr.to_string(),
-                    new_code_id: v2_core_code_id,
+                    contract_addr: module_addrs.core.to_string(),
+                    new_code_id: new_code_ids.core,
                     msg: to_binary(&dao_core::msg::MigrateMsg::FromV1 { dao_uri: None }).unwrap(),
                 }
                 .into(),
                 WasmMsg::Execute {
-                    contract_addr: core_addr.to_string(),
+                    contract_addr: module_addrs.core.to_string(),
                     msg: to_binary(&dao_core::msg::ExecuteMsg::UpdateProposalModules {
                         to_add: vec![ModuleInstantiateInfo {
                             code_id: migrator_code_id,
                             msg: to_binary(&crate::msg::InstantiateMsg {
-                                sub_daos: vec![],
+                                sub_daos: params.sub_daos.unwrap(),
                                 migration_params: MigrationParams {
-                                    migrate_stake_cw20_manager: Some(true),
+                                    migrate_stake_cw20_manager: params.migrate_cw20,
                                     close_proposal_on_execution_failure: true,
                                     pre_propose_info: dao_voting::pre_propose::PreProposeInfo::AnyoneMayPropose {},
                                 },
@@ -288,7 +163,7 @@ pub fn execute_migration(
     let perposals: cw_proposal_single_v1::query::ProposalListResponse = app
         .wrap()
         .query_wasm_smart(
-            proposal_addr.clone(),
+            module_addrs.proposal.clone(),
             &cw_proposal_single_v1::msg::QueryMsg::ReverseProposals {
                 start_before: None,
                 limit: Some(1),
@@ -299,7 +174,7 @@ pub fn execute_migration(
 
     app.execute_contract(
         sender.clone(),
-        proposal_addr.clone(),
+        module_addrs.proposal.clone(),
         &cw_proposal_single_v1::msg::ExecuteMsg::Vote {
             proposal_id,
             vote: voting_v1::Vote::Yes,
@@ -310,7 +185,7 @@ pub fn execute_migration(
 
     app.execute_contract(
         sender,
-        proposal_addr,
+        module_addrs.proposal.clone(),
         &cw_proposal_single_v1::msg::ExecuteMsg::Execute { proposal_id },
         &[],
     )
--- contracts/external/dao-migrator/src/testing/test_migration.rs
+++ contracts/external/dao-migrator/src/testing/test_migration.rs
@@ -1,77 +1,136 @@
 use std::borrow::BorrowMut;
 
-use cw_multi_test::App;
+use dao_core::query::SubDao;
 
-use crate::ContractError;
+use crate::{testing::helpers::ExecuteParams, ContractError};
 
-use super::setup::{execute_migration, init_dao_v1};
+use super::{
+    helpers::VotingType,
+    setup::{execute_migration, setup_dao_v1},
+};
 
 #[test]
 fn test_execute_migration() {
-    let app = App::default();
+    let (mut app, module_addrs, v1_code_ids) = setup_dao_v1(VotingType::Cw20);
 
-    let (mut app, core_addr, proposal_addr, v1_code_ids) = init_dao_v1(app, None);
-
-    execute_migration(
-        app.borrow_mut(),
-        core_addr.as_ref(),
-        proposal_addr.as_ref(),
-        v1_code_ids,
-    )
-    .unwrap();
+    execute_migration(app.borrow_mut(), &module_addrs, v1_code_ids, None).unwrap();
 }
 
 #[test]
 fn test_wrong_code_id() {
-    let app = App::default();
-
-    let (mut app, core_addr, proposal_addr, mut v1_code_ids) = init_dao_v1(app, None);
-
+    let (mut app, module_addrs, mut v1_code_ids) = setup_dao_v1(VotingType::Cw20);
+    let old_v1_code_ids = v1_code_ids.clone();
     v1_code_ids.proposal_single = 555;
-    let err = execute_migration(
-        app.borrow_mut(),
-        core_addr.as_ref(),
-        proposal_addr.as_ref(),
-        v1_code_ids.clone(),
-    )
-    .unwrap_err()
-    .downcast::<ContractError>()
-    .unwrap();
-    assert_eq!(err, ContractError::CantMigrateModule { code_id: 2 });
+    let err = execute_migration(app.borrow_mut(), &module_addrs, v1_code_ids, None)
+        .unwrap_err()
+        .downcast::<ContractError>()
+        .unwrap();
+    assert_eq!(
+        err,
+        ContractError::CantMigrateModule {
+            code_id: old_v1_code_ids.proposal_single
+        }
+    );
 
+    let (mut app, module_addrs, mut v1_code_ids) = setup_dao_v1(VotingType::Cw20);
+    let old_v1_code_ids = v1_code_ids.clone();
     v1_code_ids.cw20_stake = 555;
-    let err = execute_migration(
-        app.borrow_mut(),
-        core_addr.as_ref(),
-        proposal_addr.as_ref(),
-        v1_code_ids.clone(),
-    )
-    .unwrap_err()
-    .downcast::<ContractError>()
-    .unwrap();
-    assert_eq!(err, ContractError::CantMigrateModule { code_id: 4 });
+    let err = execute_migration(app.borrow_mut(), &module_addrs, v1_code_ids, None)
+        .unwrap_err()
+        .downcast::<ContractError>()
+        .unwrap();
+    assert_eq!(
+        err,
+        ContractError::CantMigrateModule {
+            code_id: old_v1_code_ids.cw20_stake
+        }
+    );
 
+    let (mut app, module_addrs, mut v1_code_ids) = setup_dao_v1(VotingType::Cw20);
     v1_code_ids.cw20_staked_balances_voting = 555;
-    let err = execute_migration(
-        app.borrow_mut(),
-        core_addr.as_ref(),
-        proposal_addr.as_ref(),
-        v1_code_ids.clone(),
-    )
-    .unwrap_err()
-    .downcast::<ContractError>()
-    .unwrap();
+    let err = execute_migration(app.borrow_mut(), &module_addrs, v1_code_ids, None)
+        .unwrap_err()
+        .downcast::<ContractError>()
+        .unwrap();
     assert_eq!(err, ContractError::VotingModuleNotFound);
 
+    let (mut app, module_addrs, mut v1_code_ids) = setup_dao_v1(VotingType::Cw4);
     v1_code_ids.cw4_voting = 555;
+    let err = execute_migration(app.borrow_mut(), &module_addrs, v1_code_ids, None)
+        .unwrap_err()
+        .downcast::<ContractError>()
+        .unwrap();
+    assert_eq!(err, ContractError::VotingModuleNotFound);
+}
+
+#[test]
+fn test_dont_migrate_cw20() {
+    let (mut app, module_addrs, v1_code_ids) = setup_dao_v1(VotingType::Cw20);
+
     let err = execute_migration(
         app.borrow_mut(),
-        core_addr.as_ref(),
-        proposal_addr.as_ref(),
-        v1_code_ids,
+        &module_addrs,
+        v1_code_ids.clone(),
+        Some(ExecuteParams {
+            sub_daos: Some(vec![]),
+            migrate_cw20: None,
+        }),
     )
     .unwrap_err()
     .downcast::<ContractError>()
     .unwrap();
-    assert_eq!(err, ContractError::VotingModuleNotFound);
+    assert_eq!(err, ContractError::DontMigrateCw20);
+
+    let err = execute_migration(
+        app.borrow_mut(),
+        &module_addrs,
+        v1_code_ids,
+        Some(ExecuteParams {
+            sub_daos: Some(vec![]),
+            migrate_cw20: Some(false),
+        }),
+    )
+    .unwrap_err()
+    .downcast::<ContractError>()
+    .unwrap();
+    assert_eq!(err, ContractError::DontMigrateCw20);
+}
+
+#[test]
+fn test_sub_daos() {
+    let (mut app, module_addrs, v1_code_ids) = setup_dao_v1(VotingType::Cw20);
+
+    execute_migration(
+        app.borrow_mut(),
+        &module_addrs,
+        v1_code_ids,
+        Some(ExecuteParams {
+            sub_daos: Some(vec![SubDao {
+                addr: "sub_dao_1".to_string(),
+                charter: None,
+            }]),
+            migrate_cw20: Some(true),
+        }),
+    )
+    .unwrap();
+
+    let sub_daos: Vec<dao_core::query::SubDao> = app
+        .wrap()
+        .query_wasm_smart(
+            module_addrs.core,
+            &dao_core::msg::QueryMsg::ListSubDaos {
+                start_after: None,
+                limit: None,
+            },
+        )
+        .unwrap();
+
+    assert_eq!(sub_daos.len(), 1);
+    assert_eq!(
+        sub_daos,
+        vec![SubDao {
+            addr: "sub_dao_1".to_string(),
+            charter: None,
+        }]
+    );
 }
