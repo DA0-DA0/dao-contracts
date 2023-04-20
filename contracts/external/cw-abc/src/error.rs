d0941fb1d670617101132dec9f152ce87d03dd9d
Jake Hartnell <jake.hartnell@gmail.com>
2023-04-19 18:55:01 -0700

    Initial commit

---
+++ contracts/external/cw-abc/src/error.rs
@@ -0,0 +1,18 @@
+use cosmwasm_std::StdError;
+use cw_utils::PaymentError;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("{0}")]
+    Payment(#[from] PaymentError),
+
+    #[error("Invalid subdenom: {subdenom:?}")]
+    InvalidSubdenom { subdenom: String },
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+}
