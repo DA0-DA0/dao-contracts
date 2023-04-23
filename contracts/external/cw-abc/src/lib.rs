aceb8ba04e065133171fe41f65f418f2f6622e5b
adairrr <32375605+adairrr@users.noreply.github.com>
2023-04-23 04:52:07 +0300

    Initial phase integration to cw-abc (#698)
    * CwAbcResult
    
    * Hatch phase configuration and init msg refactor
    
    * Initial instantiate test
    
    * Implement separate phrases and phrase configs
    
    * Remove vesting phase and update funding pool
    
    * Separate commands and queries
    
    * Update init msg with string configs
    
    * Phase config query
    
    * MinMax and config query
    
    * Add some todos
    
    * cw-ownable integration

--- contracts/external/cw-abc/src/lib.rs
+++ contracts/external/cw-abc/src/lib.rs
@@ -3,5 +3,8 @@ pub mod curves;
 mod error;
 pub mod msg;
 pub mod state;
+pub mod abc;
+pub(crate) mod commands;
+mod queries;
 
 pub use crate::error::ContractError;
--- contracts/external/cw-abc/src/state.rs
+++ contracts/external/cw-abc/src/state.rs
@@ -1,16 +1,19 @@
+use std::collections::HashSet;
 use cosmwasm_schema::cw_serde;
 
-use cosmwasm_std::Uint128;
+use cosmwasm_std::{Addr, Uint128};
 use cw_storage_plus::Item;
+use crate::abc::{ CommonsPhaseConfig, CurveType, CommonsPhase};
 
 use crate::curves::DecimalPlaces;
-use crate::msg::CurveType;
 
 /// Supply is dynamic and tracks the current supply of staked and ERC20 tokens.
 #[cw_serde]
 pub struct CurveState {
     /// reserve is how many native tokens exist bonded to the validator
     pub reserve: Uint128,
+    /// funding is how many native tokens exist unbonded and in the contract
+    pub funding: Uint128,
     /// supply is how many tokens this contract has issued
     pub supply: Uint128,
 
@@ -25,6 +28,7 @@ impl CurveState {
     pub fn new(reserve_denom: String, decimals: DecimalPlaces) -> Self {
         CurveState {
             reserve: Uint128::zero(),
+            funding: Uint128::zero(),
             supply: Uint128::zero(),
             reserve_denom,
             decimals,
@@ -36,4 +40,16 @@ pub const CURVE_STATE: Item<CurveState> = Item::new("curve_state");
 
 pub const CURVE_TYPE: Item<CurveType> = Item::new("curve_type");
 
-pub const DENOM: Item<String> = Item::new("denom");
+/// The denom used for the supply token
+pub const SUPPLY_DENOM: Item<String> = Item::new("denom");
+
+/// Keep track of who has contributed to the hatch phase
+/// TODO: cw-set?
+pub static HATCHERS: Item<HashSet<Addr>> = Item::new("hatchers");
+
+/// The phase configuration of the Augmented Bonding Curve
+pub static PHASE_CONFIG: Item<CommonsPhaseConfig<Addr>> = Item::new("phase_config");
+
+/// The phase state of the Augmented Bonding Curve
+pub static PHASE: Item<CommonsPhase> = Item::new("phase");
+
