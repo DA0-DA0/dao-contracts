1e3549b57b4930c4f1e9142407ad61c995158be2
Jake Hartnell <jake.hartnell@gmail.com>
2023-06-19 15:56:50 -0700

    Add test coverage for hooks

--- contracts/external/cw721-roles/src/tests.rs
+++ contracts/external/cw721-roles/src/tests.rs
@@ -1,5 +1,5 @@
 use cosmwasm_std::{to_binary, Addr, Binary};
-use cw4::{Member, MemberListResponse, MemberResponse, TotalWeightResponse};
+use cw4::{HooksResponse, Member, MemberListResponse, MemberResponse, TotalWeightResponse};
 use cw721::{NftInfoResponse, OwnerOfResponse};
 use cw721_base::InstantiateMsg;
 use cw_multi_test::{App, Executor};
@@ -484,4 +484,105 @@ fn test_zero_weight_token() {
     // Member query returns total weight for alice
     let member: MemberResponse = query_member(&app, &cw721_addr, ALICE, None).unwrap();
     assert_eq!(member.weight, Some(0));
+}
+
+#[test]
+fn test_hooks() {
+    let (mut app, cw721_addr) = setup();
+
+    // Mint initial NFT
+    let msg = ExecuteMsg::Mint {
+        token_id: "1".to_string(),
+        owner: ALICE.to_string(),
+        token_uri: Some("ipfs://xyz...".to_string()),
+        extension: MetadataExt {
+            role: None,
+            weight: 1,
+        },
+    };
+    app.execute_contract(Addr::unchecked(DAO), cw721_addr.clone(), &msg, &[])
+        .unwrap();
+
+    let msg = ExecuteMsg::Extension {
+        msg: ExecuteExt::AddHook {
+            addr: DAO.to_string(),
+        },
+    };
+
+    // Hook can't be added by non-minter
+    app.execute_contract(Addr::unchecked(ALICE), cw721_addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    // Hook can be added by the owner / minter
+    app.execute_contract(Addr::unchecked(DAO), cw721_addr.clone(), &msg, &[])
+        .unwrap();
+
+    // Query hooks
+    let hooks: HooksResponse = app
+        .wrap()
+        .query_wasm_smart(
+            cw721_addr.clone(),
+            &QueryMsg::Extension {
+                msg: QueryExt::Hooks {},
+            },
+        )
+        .unwrap();
+    assert_eq!(
+        hooks,
+        HooksResponse {
+            hooks: vec![DAO.to_string()]
+        }
+    );
+
+    // Test hook fires when a new member is added
+    let msg = ExecuteMsg::Mint {
+        token_id: "2".to_string(),
+        owner: ALICE.to_string(),
+        token_uri: Some("ipfs://xyz...".to_string()),
+        extension: MetadataExt {
+            role: None,
+            weight: 1,
+        },
+    };
+    // Should error as the DAO is not a contract, meaning hooks fired
+    app.execute_contract(Addr::unchecked(DAO), cw721_addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    // Should also error for burn, as this also fires hooks
+    let msg = ExecuteMsg::Burn {
+        token_id: "1".to_string(),
+    };
+    app.execute_contract(Addr::unchecked(DAO), cw721_addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    let msg = ExecuteMsg::Extension {
+        msg: ExecuteExt::RemoveHook {
+            addr: DAO.to_string(),
+        },
+    };
+
+    // Hook can't be removed by non-minter
+    app.execute_contract(Addr::unchecked(ALICE), cw721_addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    // Hook can be removed by the owner / minter
+    app.execute_contract(Addr::unchecked(DAO), cw721_addr.clone(), &msg, &[])
+        .unwrap();
+
+    // Minting should now work again as there are no hooks to dead
+    app.execute_contract(
+        Addr::unchecked(DAO),
+        cw721_addr.clone(),
+        &ExecuteMsg::Mint {
+            token_id: "2".to_string(),
+            owner: ALICE.to_string(),
+            token_uri: Some("ipfs://xyz...".to_string()),
+            extension: MetadataExt {
+                role: None,
+                weight: 1,
+            },
+        },
+        &[],
+    )
+    .unwrap();
 }
