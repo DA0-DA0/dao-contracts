a3886f36427fdfa06c501b1f6feede6c6cfeec75
Jake Hartnell <jake.hartnell@gmail.com>
2023-07-05 23:16:42 +0200

    Remove dao-dao-macros dep from dao-interface

--- packages/dao-interface/src/proposal.rs
+++ packages/dao-interface/src/proposal.rs
@@ -1,10 +1,25 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use dao_dao_macros::proposal_module_query;
+use cw2::ContractVersion;
 
-#[proposal_module_query]
+#[cw_serde]
+pub struct InfoResponse {
+    pub info: ContractVersion,
+}
+
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum Query {}
+pub enum Query {
+    /// Returns the address of the DAO this module belongs to
+    #[returns(::cosmwasm_std::Addr)]
+    Dao {},
+    /// Returns contract version info
+    #[returns(InfoResponse)]
+    Info {},
+    /// Returns the proposal ID that will be assigned to the
+    /// next proposal created.
+    #[returns(::std::primitive::u64)]
+    NextProposalId {},
+}
 
 mod tests {
     /// Make sure the enum has all of the fields we expect. This will
--- packages/dao-interface/src/voting.rs
+++ packages/dao-interface/src/voting.rs
@@ -1,14 +1,34 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Uint128;
 use cw2::ContractVersion;
-use dao_dao_macros::{active_query, token_query, voting_module_query};
 
-#[token_query]
-#[voting_module_query]
-#[active_query]
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum Query {}
+pub enum Query {
+    /// Returns the token contract address, if set.
+    #[returns(::cosmwasm_std::Addr)]
+    TokenContract {},
+    /// Returns the voting power for an address at a given height.
+    #[returns(VotingPowerAtHeightResponse)]
+    VotingPowerAtHeight {
+        address: ::std::string::String,
+        height: ::std::option::Option<::std::primitive::u64>,
+    },
+    /// Returns the total voting power at a given block heigh.
+    #[returns(TotalPowerAtHeightResponse)]
+    TotalPowerAtHeight {
+        height: ::std::option::Option<::std::primitive::u64>,
+    },
+    /// Returns the address of the DAO this module belongs to.
+    #[returns(cosmwasm_std::Addr)]
+    Dao {},
+    /// Returns contract version info.
+    #[returns(InfoResponse)]
+    Info {},
+    /// Whether the DAO is active or not.
+    #[returns(::std::primitive::bool)]
+    IsActive {},
+}
 
 #[cw_serde]
 pub struct VotingPowerAtHeightResponse {
@@ -30,33 +50,4 @@ pub struct InfoResponse {
 #[cw_serde]
 pub struct IsActiveResponse {
     pub active: bool,
-}
-
-mod tests {
-
-    /// Make sure the enum has all of the fields we expect. This will
-    /// fail to compile if not.
-    #[test]
-    fn test_macro_expansion() {
-        use cosmwasm_schema::{cw_serde, QueryResponses};
-
-        use dao_dao_macros::{active_query, token_query, voting_module_query};
-        let query = Query::TokenContract {};
-
-        #[token_query]
-        #[voting_module_query]
-        #[active_query]
-        #[cw_serde]
-        #[derive(QueryResponses)]
-        enum Query {}
-
-        match query {
-            Query::TokenContract {} => (),
-            Query::VotingPowerAtHeight { .. } => (),
-            Query::TotalPowerAtHeight { .. } => (),
-            Query::IsActive {} => (),
-            Query::Info {} => (),
-            Query::Dao {} => (),
-        }
-    }
 }
