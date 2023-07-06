3b7215022263bc2e58d18f22aa0e6f248ef7cb03
Jake Hartnell <jake.hartnell@gmail.com>
2023-07-06 14:37:39 +0200

    Publish packages script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -15,67 +15,186 @@ fi
 
 START_DIR=$(pwd)
 
-# ${f    <-- from variable f
-#   ##   <-- greedy front trim
-#   *    <-- matches anything
-#   /    <-- until the last '/'
-#  }
-# <https://stackoverflow.com/a/3162500>
+# Publishing cargo workspaces with cyclic dependencies is very tricky.
+# Dev-dependencies are often cyclic, so we need to publish without them.
+# There is ongoing discussion about this issue: https://github.com/rust-lang/cargo/issues/4242
+#
+# In the meantime, we are using cargo-hack to publish our crates.
+# This temporarily removes dev dependencies from Cargo.toml and publishes the crate.
+# https://github.com/taiki-e/cargo-hack
+#
+# Install cargo-hack to run this script. `cargo install cargo-hack`
 
-for f in ./packages/*
-do
-  echo "publishing ${f##*/}"
-  cd "$f"
-  cargo publish --dry-run
-  cd "$START_DIR"
-done
+# Crates must be published in the correct order, as some depend on others.
+# We start with publish packages, aside from dao-testing which must be published last.
 
-cd contracts/dao-dao
-cargo publish --dry-run
+# Packages
+cd packages/cw-denom
+cargo publish
 cd "$START_DIR"
 
-for f in ./contracts/voting/*
-do
-  echo "publishing ${f##*/}"
-  cd "$f"
-  cargo publish --dry-run
-  cd "$START_DIR"
-done
+cd packages/cw-hooks
+cargo publish
+cd "$START_DIR"
 
-for f in ./contracts/proposal/*
-do
-  echo "publishing ${f##*/}"
-  cd "$f"
-  cargo publish --dry-run
-  cd "$START_DIR"
-done
+cd packages/cw-wormhole
+cargo publish
+cd "$START_DIR"
 
-for f in ./contracts/staking/*
-do
-  echo "publishing ${f##*/}"
-  cd "$f"
-  cargo publish --dry-run
-  cd "$START_DIR"
-done
+cd packages/cw-stake-tracker
+cargo publish
+cd "$START_DIR"
 
-for f in ./contracts/pre-propose/*
-do
-  echo "publishing ${f##*/}"
-  cd "$f"
-  cargo publish --dry-run
-  cd "$START_DIR"
-done
+cd packages/cw-paginate-storage
+cargo publish
+cd "$START_DIR"
 
-for f in ./contracts/external/*
-do
-  echo "publishing ${f##*/}"
-  cd "$f"
-  cargo publish --dry-run
-  cd "$START_DIR"
-done
+sleep 120
+
+cd packages/cw721-controllers
+cargo publish
+cd "$START_DIR"
+
+cd packages/dao-interface
+cargo publish
+cd "$START_DIR"
+
+cd packages/dao-dao-macros
+cargo publish
+cd "$START_DIR"
+
+cd packages/dao-voting
+cargo publish
+cd "$START_DIR"
+
+cd packages/dao-vote-hooks
+cargo publish
+cd "$START_DIR"
+
+sleep 120
+
+cd packages/dao-proposal-hooks
+cargo publish
+cd "$START_DIR"
+
+cd packages/dao-pre-propose-base
+cargo publish
+cd "$START_DIR"
+
+# Test contracts
+cd test-contracts/dao-proposal-sudo
+cargo publish
+cd "$START_DIR"
+
+cd test-contracts/dao-voting-cw20-balances
+cargo publish
+cd "$START_DIR"
+
+cd test-contracts/dao-proposal-hook-counter
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+sleep 120
+
+# Contracts
+cd contracts/external/cw-token-swap
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/external/cw-fund-distributor
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/external/cw-vesting
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/external/cw-payroll-factory
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/pre-propose/dao-pre-propose-single
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+sleep 120
+
+cd contracts/pre-propose/dao-pre-propose-multiple
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/pre-propose/dao-pre-propose-approval-single
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/pre-propose/dao-pre-propose-approver
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/proposal/dao-proposal-single
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/proposal/dao-proposal-multiple
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+sleep 120
+
+cd contracts/proposal/dao-proposal-condorcet
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/staking/cw20-stake
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/staking/cw20-stake-external-rewards
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/staking/cw20-stake-reward-distributor
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/voting/dao-voting-cw4
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+sleep 120
+
+cd contracts/voting/dao-voting-cw20-staked
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/voting/dao-voting-cw721-staked
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/voting/dao-voting-native-staked
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/dao-dao
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd contracts/external/cw-admin-factory
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+sleep 120
+
+cd contracts/external/dao-migrator
+cargo hack publish --no-dev-deps --allow-dirty
+cd "$START_DIR"
+
+cd packages/dao-testing
+cargo publish
+cd "$START_DIR"
 
 echo "Everything is published!"
 
-VERSION=$(grep -A1 "\[workspace.package\]" Cargo.toml | awk -F'"' '/version/ {print $2}');
-git tag v"$VERSION"
-git push origin v"$VERSION"
+# VERSION=$(grep -A1 "\[workspace.package\]" Cargo.toml | awk -F'"' '/version/ {print $2}');
+# git tag v"$VERSION"
+# git push origin v"$VERSION"
