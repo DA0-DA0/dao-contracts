d7469ef915180eb806c996eea02cd429c234033f
Jake Hartnell <jake.hartnell@gmail.com>
2023-08-22 13:43:34 -0700

    Add name, one more test

--- contracts/external/cw-tokenfactory-issuer/tests/cases/beforesend.rs
+++ contracts/external/cw-tokenfactory-issuer/tests/cases/beforesend.rs
@@ -48,6 +48,44 @@ fn before_send_should_block_on_frozen() {
 }
 
 #[test]
+fn white_listed_addresses_can_transfer_when_token_frozen() {
+    let env = TestEnv::default();
+    let owner = &env.test_accs[0];
+    let denom = env.cw_tokenfactory_issuer.query_denom().unwrap().denom;
+    let whitelistee = &env.test_accs[1];
+    let other = &env.test_accs[2];
+
+    // freeze
+    env.cw_tokenfactory_issuer
+        .set_freezer(&owner.address(), true, owner)
+        .unwrap();
+    env.cw_tokenfactory_issuer.freeze(true, owner).unwrap();
+
+    // bank send should fail
+    let err = env
+        .send_tokens(whitelistee.address(), coins(10000, denom.clone()), owner)
+        .unwrap_err();
+    assert_eq!(err, RunnerError::ExecuteError { msg:  format!("failed to execute message; message index: 0: failed to call before send hook for denom {denom}: The contract is frozen for denom \"{denom}\": execute wasm contract failed") });
+
+    // Whitelist address
+    env.cw_tokenfactory_issuer
+        .set_whitelister(&owner.address(), true, owner)
+        .unwrap();
+    env.cw_tokenfactory_issuer
+        .whitelist(&whitelistee.address(), true, owner)
+        .unwrap();
+
+    // bank send should pass
+    env.send_tokens(other.address(), coins(10000, denom.clone()), whitelistee)
+        .unwrap_err();
+    // Non whitelisted address can't transfer, bank send should fail
+    let err = env
+        .send_tokens(other.address(), coins(10000, denom.clone()), owner)
+        .unwrap_err();
+    assert_eq!(err, RunnerError::ExecuteError { msg:  format!("failed to execute message; message index: 0: failed to call before send hook for denom {denom}: The contract is frozen for denom \"{denom}\": execute wasm contract failed") });
+}
+
+#[test]
 fn before_send_should_block_sending_from_blacklisted_address() {
     let env = TestEnv::default();
     let owner = &env.test_accs[0];
