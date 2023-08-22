d04f39a38eb1a783d99b93727b8097d1608e96b6
Jake Hartnell <jake.hartnell@gmail.com>
2023-08-21 19:10:04 -0700

    Add test-tube feature to dao-testing

--- packages/dao-testing/src/test_tube/mod.rs
+++ packages/dao-testing/src/test_tube/mod.rs
@@ -2,4 +2,8 @@
 // and also, tarpaulin will not be able read coverage out of wasm binary anyway
 #![cfg(not(tarpaulin))]
 
+// Integrationg tests using an actual chain binary, requires
+// the "test-tube" feature to be enabled
+// cargo test --features test-tube
+#[cfg(feature = "test-tube")]
 pub mod cw_tokenfactory_issuer;
