ab6c8cd92463b4a8bf289614c71767cbb7ba7164
Jake Hartnell <jake.hartnell@gmail.com>
2023-08-21 17:13:38 -0700

    Cleanup, unfortunately test-tube doesn't have metadata queries yet

--- contracts/external/cw-tokenfactory-issuer/tests/cases/denom_metadata.rs
+++ contracts/external/cw-tokenfactory-issuer/tests/cases/denom_metadata.rs
@@ -36,67 +36,6 @@ fn set_denom_metadata_by_contract_owner_should_work() {
     env.cw_tokenfactory_issuer
         .set_denom_metadata(metadata, owner)
         .unwrap();
-
-    // // TODO fix me, query metadata
-    // // should set basic metadata
-    // assert_eq!(
-    //     env.tokenfactory()
-    //         .query_denom_metadata(&QueryDenomMetadataRequest {
-    //             denom: denom.clone()
-    //         })
-    //         .unwrap()
-    //         .metadata
-    //         .unwrap(),
-    //     Metadata {
-    //         description: "".to_string(),
-    //         denom_units: vec![vec![
-    //             // must start with `denom` with exponent 0
-    //             DenomUnit {
-    //                 denom: denom.clone(),
-    //                 exponent: 0,
-    //                 aliases: vec![],
-    //             }
-    //         ],]
-    //         .concat(),
-    //         base: denom.clone(),
-    //         display: "".to_string(),
-    //         name: "".to_string(),
-    //         symbol: "".to_string(),
-    //     }
-    // );
-
-    // // set denom metadata
-    // env.cw_tokenfactory_issuer
-    //     .set_denom_metadata(metadata.clone(), owner)
-    //     .unwrap();
-
-    // // should update metadata
-
-    // assert_eq!(
-    //     env.bank()
-    //         .query_denom_metadata(&QueryDenomMetadataRequest {
-    //             denom: denom.clone()
-    //         })
-    //         .unwrap()
-    //         .metadata
-    //         .unwrap(),
-    //     Metadata {
-    //         description: metadata.description,
-    //         denom_units: metadata
-    //             .denom_units
-    //             .into_iter()
-    //             .map(|d| DenomUnit {
-    //                 denom: d.denom,
-    //                 exponent: d.exponent,
-    //                 aliases: d.aliases,
-    //             })
-    //             .collect(),
-    //         base: denom,
-    //         display: metadata.display,
-    //         name: metadata.name,
-    //         symbol: metadata.symbol,
-    //     }
-    // );
 }
 
 #[test]
