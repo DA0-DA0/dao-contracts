e6e3a5b1fa7cd17a12f6e6a424dceab24f9de1a3
Noah Saso <noahsaso@gmail.com>
2023-11-17 09:47:52 -0800

    Add another missing approver query to query the approver proposal ID using the pre propose approval ID.

--- contracts/pre-propose/dao-pre-propose-approver/src/state.rs
+++ contracts/pre-propose/dao-pre-propose-approver/src/state.rs
@@ -4,4 +4,6 @@ use cw_storage_plus::{Item, Map};
 // Stores the address of the pre-propose approval contract
 pub const PRE_PROPOSE_APPROVAL_CONTRACT: Item<Addr> = Item::new("pre_propose_approval_contract");
 // Maps proposal ids to pre-propose ids
-pub const PROPOSAL_ID_TO_PRE_PROPOSE_ID: Map<u64, u64> = Map::new("proposal_ids");
+pub const PROPOSAL_ID_TO_PRE_PROPOSE_ID: Map<u64, u64> = Map::new("proposal_to_pre_propose");
+// Maps pre-propose ids to proposal ids
+pub const PRE_PROPOSE_ID_TO_PROPOSAL_ID: Map<u64, u64> = Map::new("pre_propose_to_proposal");
