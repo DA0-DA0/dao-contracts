a20e72bdd2fea23e045ce00e08a52c412e6a5c8d
Jake Hartnell <jake.hartnell@gmail.com>
2024-01-03 20:35:10 -0800

    More code comments

--- contracts/distribution/dao-rewards-distributor/src/error.rs
+++ contracts/distribution/dao-rewards-distributor/src/error.rs
@@ -5,24 +5,31 @@ use thiserror::Error;
 pub enum ContractError {
     #[error(transparent)]
     Std(#[from] StdError),
+
     #[error(transparent)]
     Ownable(#[from] cw_ownable::OwnershipError),
+
     #[error(transparent)]
     Cw20Error(#[from] cw20_base::ContractError),
-    #[error("Staking change hook sender is not staking contract")]
-    InvalidHookSender {},
-    #[error("No rewards claimable")]
-    NoRewardsClaimable {},
-    #[error("Reward period not finished")]
-    RewardPeriodNotFinished {},
-    #[error("Invalid funds")]
-    InvalidFunds {},
+
     #[error("Invalid Cw20")]
     InvalidCw20 {},
+
+    #[error("Invalid funds")]
+    InvalidFunds {},
+
+    #[error("Staking change hook sender is not staking contract")]
+    InvalidHookSender {},
+
+    #[error("No rewards claimable")]
+    NoRewardsClaimable {},
+
+    #[error("Reward period not finished")]
+    RewardPeriodNotFinished {},
+
     #[error("Reward rate less then one per block")]
     RewardRateLessThenOnePerBlock {},
+
     #[error("Reward duration can not be zero")]
     ZeroRewardDuration {},
-    #[error("can not migrate. current version is up to date")]
-    AlreadyMigrated {},
 }
--- contracts/distribution/dao-rewards-distributor/src/state.rs
+++ contracts/distribution/dao-rewards-distributor/src/state.rs
@@ -1,18 +1,19 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Uint128, Uint256};
 use cw20::Denom;
-
 use cw_storage_plus::{Item, Map};
 
 #[cw_serde]
 pub struct Config {
+    /// The address of a DAO DAO voting power module contract.
     pub vp_contract: Addr,
+    /// An optional contract that is allowed to call the StakeChangedHook in
+    /// place of the voting power contract.
     pub hook_caller: Option<Addr>,
+    /// The Denom in which rewards are paid out.
     pub reward_token: Denom,
 }
-
-// `"config"` key stores v1 configuration.
-pub const CONFIG: Item<Config> = Item::new("config_v2");
+pub const CONFIG: Item<Config> = Item::new("config");
 
 #[cw_serde]
 pub struct RewardConfig {
@@ -26,6 +27,9 @@ pub const REWARD_PER_TOKEN: Item<Uint256> = Item::new("reward_per_token");
 
 pub const LAST_UPDATE_BLOCK: Item<u64> = Item::new("last_update_block");
 
+/// A map of user addresses to their pending rewards.
 pub const PENDING_REWARDS: Map<Addr, Uint128> = Map::new("pending_rewards");
 
+/// A map of user addresses to their rewards per token. In other words, it is the
+/// reward per share of voting power that the user has.
 pub const USER_REWARD_PER_TOKEN: Map<Addr, Uint256> = Map::new("user_reward_per_token");
