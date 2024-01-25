c1ef0bf30403a62613d012c7ec534ec37f2c530b
Jake Hartnell <jake.hartnell@gmail.com>
2024-01-25 15:57:30 +0100

    Proposal incentives fixups and notes

--- contracts/external/dao-proposal-incentives/src/error.rs
+++ contracts/external/dao-proposal-incentives/src/error.rs
@@ -1,5 +1,5 @@
-use cosmwasm_std::StdError;
-use cw_utils::ParseReplyError;
+use cosmwasm_std::{StdError, Uint128};
+use cw_utils::{ParseReplyError, PaymentError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -7,11 +7,17 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("Unauthorized")]
-    Unauthorized {},
+    #[error("{0}")]
+    PaymentError(#[from] PaymentError),
+
+    #[error("You need to deposit enough incentives for at least one epoch of incentives. Expected {expected}, got {actual}.")]
+    InsufficientInitialDeposit { expected: Uint128, actual: Uint128 },
 
     #[error("{0}")]
     ParseReplyError(#[from] ParseReplyError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
 
     #[error("An unknown reply ID was received.")]
     UnknownReplyID {},
