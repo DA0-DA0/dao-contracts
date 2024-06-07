0cc8ae9656a5d614c59e89bf1436f2c65b730a13
Noah Saso <noahsaso@gmail.com>
2024-06-07 16:40:53 -0400

    started adding x/onft staking module

---
+++ contracts/voting/dao-voting-onft-staked/src/lib.rs
@@ -0,0 +1,13 @@
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+
+pub mod contract;
+mod error;
+pub mod msg;
+mod omniflix;
+pub mod state;
+use osmosis_std::shim;
+
+// #[cfg(test)]
+// mod testing;
+
+pub use crate::error::ContractError;
