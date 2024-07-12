2216c70b4886d9afacfc016d11bed0bab2c4f4f6
Noah Saso <noahsaso@gmail.com>
2024-07-12 12:41:17 -0500

    started adding instantiate2 support to admin factory

--- contracts/external/cw-admin-factory/src/state.rs
+++ contracts/external/cw-admin-factory/src/state.rs
@@ -3,3 +3,6 @@ use cw_storage_plus::Item;
 
 /// The account allowed to execute the contract. If None, anyone is allowed.
 pub const ADMIN: Item<Option<Addr>> = Item::new("admin");
+
+/// The expected instantiate2 address to validate in the reply.
+pub const EXPECT: Item<Addr> = Item::new("expect");
