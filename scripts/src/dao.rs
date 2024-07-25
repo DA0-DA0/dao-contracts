3990e13c3d64dc3dad6deafab08e2eb3910587ca
hard-nett <hardnettt@proton.me>
2024-07-25 02:08:47 +0000

    dao cw-orch interface

--- scripts/src/dao.rs
+++ scripts/src/dao.rs
@@ -1,27 +1,39 @@
 use cw_orch::prelude::*;
-use dao_cw_orch::{DaoDaoCore, DaoProposalSingle, DaoProposalSudo};
+use dao_cw_orch::DaoDaoCore;
 
-// minimal dao
+use crate::{
+    DaoDistributionSuite, DaoExternalSuite, DaoProposalSuite, DaoStakingSuite, DaoVotingSuite,
+};
+
+// full dao suite
 pub struct DaoDao<Chain> {
     pub dao_core: DaoDaoCore<Chain>,
-    pub dao_proposal_single: DaoProposalSingle<Chain>,
-    pub dao_proposal_sudo: DaoProposalSudo<Chain>,
+    pub proposal_suite: DaoProposalSuite<Chain>,
+    pub voting_suite: DaoVotingSuite<Chain>,
+    pub staking_suite: DaoStakingSuite<Chain>,
+    pub distribution_suite: DaoDistributionSuite<Chain>,
+    pub external_suite: DaoExternalSuite<Chain>,
 }
 
 impl<Chain: CwEnv> DaoDao<Chain> {
     pub fn new(chain: Chain) -> DaoDao<Chain> {
         DaoDao::<Chain> {
             dao_core: DaoDaoCore::new("dao_dao_core", chain.clone()),
-            dao_proposal_single: DaoProposalSingle::new("dao_proposal_single", chain.clone()),
-            dao_proposal_sudo: DaoProposalSudo::new("dao_proposal_sudo", chain.clone()),
+            proposal_suite: DaoProposalSuite::new(chain.clone()),
+            voting_suite: DaoVotingSuite::new(chain.clone()),
+            staking_suite: DaoStakingSuite::new(chain.clone()),
+            distribution_suite: DaoDistributionSuite::new(chain.clone()),
+            external_suite: DaoExternalSuite::new(chain.clone()),
         }
     }
 
     pub fn upload(&self) -> Result<(), CwOrchError> {
         self.dao_core.upload()?;
-        self.dao_proposal_single.upload()?;
-        self.dao_proposal_sudo.upload()?;
-
+        self.proposal_suite.upload()?;
+        self.voting_suite.upload()?;
+        self.staking_suite.upload()?;
+        self.distribution_suite.upload()?;
+        self.external_suite.upload()?;
         Ok(())
     }
 }
--- scripts/src/tests/deploy/external.rs
+++ scripts/src/tests/deploy/external.rs
@@ -1,288 +1,43 @@
 use crate::external::*;
-use btsg_ft_factory::msg::InstantiateMsg;
 use cw_orch::prelude::*;
 
 // admin factory
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for AdminFactorySuite<Chain> {
+impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for DaoExternalSuite<Chain> {
     // We don't have a custom error type
     type Error = CwOrchError;
     type DeployData = Addr;
 
     fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let factory = AdminFactorySuite::new(chain.clone());
-        factory.upload()?;
-        Ok(factory)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.factory)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let factory = Self::new(chain.clone());
-        Ok(factory)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: AdminFactorySuite<Chain> = AdminFactorySuite::store_on(chain.clone())?;
-        suite.factory.instantiate(
-            &cw_admin_factory::msg::InstantiateMsg { admin: None },
-            None,
-            None,
-        )?;
+        let suite = DaoExternalSuite::new(chain.clone());
+        suite.upload()?;
         Ok(suite)
     }
-}
-
-// payroll
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for PayrollSuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let payroll = PayrollSuite::new(chain.clone());
-        payroll.upload()?;
-
-        Ok(payroll)
-    }
 
     fn deployed_state_file_path() -> Option<String> {
         None
     }
 
     fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.payroll)]
+        vec![
+            Box::new(&mut self.admin_factory),
+            Box::new(&mut self.btsg_ft_factory),
+            Box::new(&mut self.payroll_factory),
+            Box::new(&mut self.cw_tokenswap),
+            Box::new(&mut self.cw_tokenfactory_issuer),
+            Box::new(&mut self.cw_vesting),
+            Box::new(&mut self.cw721_roles),
+            Box::new(&mut self.migrator),
+        ]
     }
 
     fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let payroll = Self::new(chain.clone());
-        Ok(payroll)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: PayrollSuite<Chain> = PayrollSuite::store_on(chain.clone())?;
-        // ########### Instantiate ##############
-        let _init = suite.payroll.instantiate(
-            &cw_payroll_factory::msg::InstantiateMsg {
-                owner: Some(chain.sender_addr().to_string()),
-                vesting_code_id: suite.vesting.code_id()?,
-            },
-            None,
-            None,
-        )?;
-
+        let suite = Self::new(chain.clone());
         Ok(suite)
     }
-}
-
-// tokenswap
-
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for TokenSwapSuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let tokenswap = TokenSwapSuite::new(chain.clone());
-        tokenswap.upload()?;
-        Ok(tokenswap)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.tokenswap)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let tokenswap = Self::new(chain.clone());
-        Ok(tokenswap)
-    }
 
     fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
         // ########### Upload ##############
-        let suite: TokenSwapSuite<Chain> = TokenSwapSuite::store_on(chain.clone())?;
-        Ok(suite)
-    }
-}
-
-// tokenfactory issuer
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for TokenFactorySuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let factory = TokenFactorySuite::new(chain.clone());
-        factory.upload()?;
-        Ok(factory)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.tokenfactory)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let factory = Self::new(chain.clone());
-        Ok(factory)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: TokenFactorySuite<Chain> = TokenFactorySuite::store_on(chain.clone())?;
-
-        // ########### Instantiate ##############
-        // let init = TokenfactoryIssuerInit::NewToken {
-        //     subdenom: "DAOTOKEN".into(),
-        // };
-        // suite.tokenfactory.instantiate(&init, None, None)?;
-
-        Ok(suite)
-    }
-}
-
-// cw-vesting
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for VestingSuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let vesting = VestingSuite::new(chain.clone());
-        vesting.upload()?;
-        Ok(vesting)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.vesting)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let vesting = Self::new(chain.clone());
-        Ok(vesting)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: VestingSuite<Chain> = VestingSuite::store_on(chain.clone())?;
-        Ok(suite)
-    }
-}
-
-// cw721-roles
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for Cw721RolesSuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let roles = Cw721RolesSuite::new(chain.clone());
-        roles.upload()?;
-        Ok(roles)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.roles)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let roles = Self::new(chain.clone());
-        Ok(roles)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: Cw721RolesSuite<Chain> = Cw721RolesSuite::store_on(chain.clone())?;
-        // ########### Instantiate ##############
-        Ok(suite)
-    }
-}
-
-// dao-migrator
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for DaoMigrationSuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let migrator = DaoMigrationSuite::new(chain.clone());
-        migrator.upload()?;
-        Ok(migrator)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.migrator)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let migrator = Self::new(chain.clone());
-        Ok(migrator)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: DaoMigrationSuite<Chain> = DaoMigrationSuite::store_on(chain.clone())?;
-        // ########### Instantiate ##############
-
-        Ok(suite)
-    }
-}
-
-// bitsong fantoken factory
-impl<Chain: CwEnv> cw_orch::contract::Deploy<Chain> for FantokenFactorySuite<Chain> {
-    // We don't have a custom error type
-    type Error = CwOrchError;
-    type DeployData = Addr;
-
-    fn store_on(chain: Chain) -> Result<Self, Self::Error> {
-        let factory = FantokenFactorySuite::new(chain.clone());
-        factory.upload()?;
-        Ok(factory)
-    }
-
-    fn deployed_state_file_path() -> Option<String> {
-        None
-    }
-
-    fn get_contracts_mut(&mut self) -> Vec<Box<&mut dyn ContractInstance<Chain>>> {
-        vec![Box::new(&mut self.factory)]
-    }
-
-    fn load_from(chain: Chain) -> Result<Self, Self::Error> {
-        let factory = Self::new(chain.clone());
-        Ok(factory)
-    }
-
-    fn deploy_on(chain: Chain, _data: Self::DeployData) -> Result<Self, Self::Error> {
-        // ########### Upload ##############
-        let suite: FantokenFactorySuite<Chain> = FantokenFactorySuite::store_on(chain.clone())?;
-        // ########### Instantiate ##############
-        suite.factory.instantiate(&InstantiateMsg {}, None, None)?;
+        let suite: DaoExternalSuite<Chain> = DaoExternalSuite::store_on(chain.clone())?;
         Ok(suite)
     }
 }
