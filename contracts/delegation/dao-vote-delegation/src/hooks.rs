f0ca08355bafa769b28489c214cb79233efb6288
bekauz <bekauz@protonmail.com>
2025-03-27 23:13:49 +0100

    wip: hooks rework

--- contracts/delegation/dao-vote-delegation/src/hooks.rs
+++ contracts/delegation/dao-vote-delegation/src/hooks.rs
@@ -27,11 +27,11 @@ pub(crate) fn execute_stake_changed(
         return Err(ContractError::UnauthorizedHookCaller {});
     }
 
-    let addr = match msg {
-        StakeChangedHookMsg::Stake { addr, .. } | StakeChangedHookMsg::Unstake { addr, .. } => addr,
-    };
-
-    handle_voting_power_changed_hook(deps, &env, addr)
+    match msg {
+        StakeChangedHookMsg::Stake { addr, .. } | StakeChangedHookMsg::Unstake { addr, .. } => {
+            handle_voting_power_changed_hook(deps, &env, addr)
+        }
+    }
 }
 
 pub(crate) fn execute_membership_changed(
@@ -65,12 +65,12 @@ pub(crate) fn execute_nft_stake_changed(
         return Err(ContractError::UnauthorizedHookCaller {});
     }
 
-    let addr = match msg {
+    match msg {
         NftStakeChangedHookMsg::Stake { addr, .. }
-        | NftStakeChangedHookMsg::Unstake { addr, .. } => addr,
-    };
-
-    handle_voting_power_changed_hook(deps, &env, addr)
+        | NftStakeChangedHookMsg::Unstake { addr, .. } => {
+            handle_voting_power_changed_hook(deps, &env, addr)
+        }
+    }
 }
 
 /// Perform necessary updates when a member's voting power changes.
@@ -99,16 +99,21 @@ pub(crate) fn handle_voting_power_changed_hook(
         Some(env.block.height + 1),
     )?;
 
+    // depending on whether the voting power hook was fired for a delegate or delegator,
+    // we need to handle the voting power change differently.
     // check latest state instead of historical height, since we need access to
     // immediate updates made earlier in the same block
-    match is_delegate_registered(deps.as_ref(), &addr, None)? {
-        true => handle_delegate_voting_power_changed_hook(deps, env.block.height, addr, new_vp),
+    if is_delegate_registered(deps.as_ref(), &addr, None)? {
+        handle_delegate_voting_power_changed_hook(deps, env.block.height, addr, new_vp)
+    } else {
         // if not a delegate, check if they have any delegations, and update
         // delegate VPs accordingly
-        false => handle_delegator_voting_power_changed_hook(deps, env, dao, addr, new_vp),
+        handle_delegator_voting_power_changed_hook(deps, env, dao, addr, new_vp)
     }
 }
 
+/// handles the delegate voting power changed hook by unregistering the delegate
+/// if they no longer have any voting power.
 fn handle_delegate_voting_power_changed_hook(
     deps: DepsMut,
     block_height: u64,
@@ -125,6 +130,7 @@ fn handle_delegate_voting_power_changed_hook(
         .add_attribute("member_type", "delegate"))
 }
 
+/// handles the delegator voting power changed hook by updating
 fn handle_delegator_voting_power_changed_hook(
     deps: DepsMut,
     env: &Env,
@@ -137,7 +143,12 @@ fn handle_delegator_voting_power_changed_hook(
     let delegations = DELEGATIONS.load_all_latest(deps.storage, &delegator, env.block.height)?;
 
     let config = CONFIG.load(deps.storage)?;
-    let old_vp = dao_voting::voting::get_voting_power(deps.as_ref(), delegator, &dao, None)?;
+    let old_vp = dao_voting::voting::get_voting_power(
+        deps.as_ref(),
+        delegator,
+        &dao,
+        Some(env.block.height),
+    )?;
 
     for LoadedItem {
         item: Delegation { delegate, percent },
@@ -194,7 +205,6 @@ pub fn execute_vote_hook(
             proposal_id,
             voter,
             power,
-            height,
             is_first_vote,
             ..
         } => {
@@ -202,37 +212,57 @@ pub fn execute_vote_hook(
             // delegates by subtracting this member's delegated VP. if not first
             // vote, this has already been done.
             if is_first_vote {
-                let delegator = deps.api.addr_validate(&voter)?;
-                let delegates = DELEGATIONS.load_all(deps.storage, &delegator, env.block.height)?;
-                for LoadedItem {
-                    item: Delegation { delegate, percent },
-                    ..
-                } in delegates
-                {
-                    let udvp = get_udvp(
-                        deps.as_ref(),
-                        &delegate,
-                        &proposal_module,
-                        proposal_id,
-                        height,
-                    )?;
-
-                    let delegated_vp = calculate_delegated_vp(power, percent);
-
-                    // remove the delegator's delegated VP from the delegate's
-                    // unvoted delegated VP for this proposal since this
-                    // delegator just voted.
-                    let new_udvp = udvp.checked_sub(delegated_vp)?;
-
-                    UNVOTED_DELEGATED_VP.save(
-                        deps.storage,
-                        (&delegate, &proposal_module, proposal_id),
-                        &new_udvp,
-                    )?;
-                }
+                handle_first_delegator_vote(
+                    deps,
+                    voter,
+                    env.block.height,
+                    power,
+                    proposal_id,
+                    &proposal_module,
+                )?;
             }
         }
     }
 
     Ok(Response::new().add_attribute("action", "vote_hook"))
+}
+
+fn handle_first_delegator_vote(
+    deps: DepsMut,
+    voter: String,
+    env_block_height: u64,
+    power: Uint128,
+    proposal_id: u64,
+    proposal_module: &Addr,
+) -> Result<(), ContractError> {
+    let delegator = deps.api.addr_validate(&voter)?;
+    let delegates = DELEGATIONS.load_all(deps.storage, &delegator, env_block_height)?;
+    for LoadedItem {
+        item: Delegation { delegate, percent },
+        ..
+    } in delegates
+    {
+        let udvp = get_udvp(
+            deps.as_ref(),
+            &delegate,
+            proposal_module,
+            proposal_id,
+            env_block_height,
+        )?;
+
+        let delegated_vp = calculate_delegated_vp(power, percent);
+
+        // remove the delegator's delegated VP from the delegate's
+        // unvoted delegated VP for this proposal since this
+        // delegator just voted.
+        let new_udvp = udvp.checked_sub(delegated_vp)?;
+
+        UNVOTED_DELEGATED_VP.save(
+            deps.storage,
+            (&delegate, proposal_module, proposal_id),
+            &new_udvp,
+        )?;
+    }
+
+    Ok(())
 }
