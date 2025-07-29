8aa6819d10b1526d1ddebe094620562c3137d97b
bekauz <bekauz@protonmail.com>
2025-07-29 23:04:19 +0200

    add module init helpers

--- contracts/external/dao-rbam/src/helpers.rs
+++ contracts/external/dao-rbam/src/helpers.rs
@@ -1,8 +1,8 @@
-use cosmwasm_std::{DepsMut, Env, StdResult};
+use cosmwasm_std::{to_json_binary, Addr, DepsMut, Env, StdResult, SubMsg};
+use dao_interface::state::{Admin, ModuleInstantiateInfo, ModuleUpdate};
 
 use crate::{
-    state::{ENABLED, NEXT_ID},
-    ContractError,
+    contract::{INSTANTIATE_FILTER_REPLY_ID, INSTANTIATE_PROTOBUF_REGISTRY_REPLY_ID}, state::{ENABLED, NEXT_ID}, ContractError
 };
 
 /// Ensure the RBAM system is enabled.
@@ -26,4 +26,67 @@ pub fn get_next_id(deps: DepsMut) -> StdResult<u64> {
 pub fn get_module_label(env: &Env, suffix: &str) -> String {
     let last6 = env.contract.address.to_string().chars().rev().take(6).collect::<String>();
     format!("rbam-{}-{}", last6, suffix)
+}
+
+pub fn submsg_instantiate_registry(
+    env: &Env,
+    owner: String,
+    code_id: u64,
+    salt: Option<cosmwasm_std::Binary>,
+) -> Result<SubMsg, ContractError> {
+    let submsg = SubMsg::reply_on_success(
+        ModuleInstantiateInfo {
+            code_id,
+            // Set this RBAM as owner so we can prepare protobuf messages.
+            msg: to_json_binary(&cw_protobuf_registry::msg::InstantiateMsg {
+                owner: Some(env.contract.address.to_string()),
+            })?,
+            // Set RBAM's owner as the admin so they can upgrade it.
+            admin: Some(Admin::Address {
+                addr: owner,
+            }),
+            salt,
+            funds: None,
+            label: get_module_label(&env, "protobuf-registry")
+        }
+        .into_cosmos_msg(""),
+        INSTANTIATE_PROTOBUF_REGISTRY_REPLY_ID,
+    );
+
+    Ok(submsg)
+}
+
+
+pub fn submsg_instantiate_filter(
+    env: &Env,
+    owner: String,
+    code_id: u64,
+    salt: Option<cosmwasm_std::Binary>,
+    protobuf_registry: Option<Addr>,
+) -> Result<SubMsg, ContractError> {
+    let protobuf_registry = protobuf_registry.map(|addr| ModuleUpdate::Existing { address: addr.to_string() });
+
+    let module_init_msg = ModuleInstantiateInfo {
+        code_id,
+        // Set RBAM's owner as owner.
+        msg: to_json_binary(&cw_filter::msg::InstantiateMsg {
+            owner: Some(owner.to_string()),
+            protobuf_registry,
+        })?,
+        // Set RBAM's owner as the admin so they can upgrade it.
+        admin: Some(Admin::Address {
+            addr: owner.to_string(),
+        }),
+        salt,
+        funds: None,
+        label: get_module_label(&env, "filter"),
+    }
+    .into_cosmos_msg("");
+
+    let submsg = SubMsg::reply_on_success(
+        module_init_msg,
+        INSTANTIATE_FILTER_REPLY_ID,
+    );
+
+    Ok(submsg)
 }
